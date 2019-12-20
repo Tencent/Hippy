f919f7227edb300baab9ca6aa549d80ec4f6e84e
mengyanluo <mengyanluo@tencent.com>
2019-12-20 15:50:09 +0800

    chore(doc): change license style

--- ios/sdk/component/listview/HippyBaseListItemViewManager.h
+++ ios/sdk/component/listview/HippyBaseListItemViewManager.h
@@ -1,10 +1,24 @@
-//
-//  HippyBaseListItemViewManager.h
-//  QBCommonRNLib
-//
-//  Created by pennyli on 2018/8/28.
-//  Copyright © 2018年 刘海波. All rights reserved.
-//
+/*!
+* iOS SDK
+*
+* Tencent is pleased to support the open source community by making
+* Hippy available.
+*
+* Copyright (C) 2019 THL A29 Limited, a Tencent company.
+* All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 #import "HippyViewManager.h"
 
