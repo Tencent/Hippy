7e21cc55c69dcafb0948283babd091130f6c7308
mengyanluo <mengyanluo@tencent.com>
2019-12-27 16:12:54 +0800

    feat(demo): add local debug and MyView demo

---
+++ examples/ios-demo/HippyDemo/TestModule.h
@@ -0,0 +1,13 @@
+//
+//  TestModule.h
+//  React
+//
+//  Created by mengyanluo on 2018/6/20.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "HippyBridgeModule.h"
+
+@interface TestModule : NSObject <HippyBridgeModule>
+
+@end
