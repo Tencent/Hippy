2346d2c368a6119d41655fbdcb0cb8aaebcb874b
kassadin <kassadin@foxmail.com>
2019-12-30 10:59:27 +0800

    style: fix lint warnings

--- scripts/build-example.js
+++ scripts/build-example.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs-extra');
@@ -6,26 +7,27 @@ const example = process.argv[2];
 console.log(example);
 console.log(process.cwd());
 
-var BASE_PATH = process.cwd();
+const BASE_PATH = process.cwd();
 // Target demo project path
-var DEMO_PATH = path.join(BASE_PATH, "examples", example);
-console.log(DEMO_PATH)
+const DEMO_PATH = path.join(BASE_PATH, 'examples', example);
+console.log(DEMO_PATH);
 if (!fs.existsSync(DEMO_PATH)) {
   console.log(`❌ Can not find demo project ${example}`);
-  return
+  return;
 }
 
 process.chdir(DEMO_PATH);
 
+const execOptions = { stdio: 'inherit' };
 console.log(`1/3 Start to install ${example} dependencies`);
-execSync('npm  install');
+execSync('npm  install', execOptions);
 
 console.log(`2/3 Start to build project ${example}`);
-execSync('npm run hippy:vendor'); // Build vendor js
-execSync('npm run hippy:build'); // Build index js
+execSync('npm run hippy:vendor', execOptions); // Build vendor js
+execSync('npm run hippy:build', execOptions); // Build index js
 
-console.log(`3/3 Copy the built files to native`);
-fs.copySync('./dist/ios/', '../ios-demo/res/') // Update the ios demo project
-fs.copySync('./dist/android', '../android-demo/res/') // # Update the android project
+console.log('3/3 Copy the built files to native');
+fs.copySync('./dist/ios/', '../ios-demo/res/'); // Update the ios demo project
+fs.copySync('./dist/android', '../android-demo/res/'); // # Update the android project
 
-console.log(`👌 All done, you can open your native app now, enjoy.`);
+console.log('👌 All done, you can open your native app now, enjoy.');
