24b395b9ed00a52ede4e7b054397d2edc7449635
XQ Kuang <x@kxq.io>
2020-01-21 19:00:56 +0800

    chore(react): fixed MyView changeColor execution issue

--- examples/hippy-react-demo/src/externals/MyView/index.jsx
+++ examples/hippy-react-demo/src/externals/MyView/index.jsx
@@ -5,18 +5,23 @@ import MyView from './MyView';
 // 使用自定义的View
 export default class MyViewDemo extends React.Component {
   componentDidMount() {
-    const self = this;
     // 调用控件扩展的方法，不过一般不这么做都是扩展在属性里面
     setTimeout(() => {
-      self.refs.myview.changeColor('#0055f0');
+      this.myview.changeColor('#0055f0');
     }, 1000);
   }
 
   render() {
+    // 要注意，Hippy 终端只支持加载 Unicode 编码的代码，所以代码都通过 unicode-loader 转换了一次编码。
+    // 所以中文如果直接写到 props 会变成 \uxxxx 的乱码。
+    // 有两个办法：
+    // 1. 将中文写成单独变量。
+    // 2. 加载时通过 [unicodeToChar](https://github.com/Tencent/Hippy/blob/master/packages/hippy-react/src/components/text.tsx#L84) 转一下
+    const text = '你好，我是MyView';
     return (
       <MyView
         ref={(ref) => { this.myview = ref; }}
-        text="你好，我是MyView"
+        text={text}
         style={{ width: 250, height: 100, color: 'black' }}
       >
         <Text style={{
