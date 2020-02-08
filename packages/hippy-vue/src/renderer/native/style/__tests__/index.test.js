00661f8c22beded97cb5e2a6463b50296fb7e6c5
jeromehan <jeromehan@163.com>
2020-02-08 17:45:01 +0800

    fix(hippy-vue): fixed css selectors TypeSelector match

--- packages/hippy-vue/src/renderer/native/style/__tests__/index.test.js
+++ packages/hippy-vue/src/renderer/native/style/__tests__/index.test.js
@@ -16,10 +16,15 @@ test('IdSelector test', (t) => {
   const matchedCSS = cssMap.query(node);
   t.is(matchedCSS.selectors.length, 7);
 });
-
 test('ClassSelector test', (t) => {
   const node = new ElementNode('div');
   node.setAttribute('class', 'class');
   const matchedCSS = cssMap.query(node);
   t.is(matchedCSS.selectors.length, 5);
+});
+test('TypeSelector test', (t) => {
+  const node = new ElementNode('tag');
+  node.setStyle('color', 'red');
+  const matchedCSS = cssMap.query(node);
+  t.is(matchedCSS.selectors.length, 4);
 });
