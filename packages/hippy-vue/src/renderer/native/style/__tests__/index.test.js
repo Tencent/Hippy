e85ca9e93048fcb67ea7ea7fb406bb24467d005c
jeromehan <jeromehan@163.com>
2020-02-08 17:50:24 +0800

    fix(hippy-vue): add TypeSelector test

--- packages/hippy-vue/src/renderer/native/style/__tests__/index.test.js
+++ packages/hippy-vue/src/renderer/native/style/__tests__/index.test.js
@@ -23,3 +23,10 @@ test('ClassSelector test', (t) => {
   const matchedCSS = cssMap.query(node);
   t.is(matchedCSS.selectors.length, 5);
 });
+
+test('TypeSelector test', (t) => {
+  const node = new ElementNode('tag');
+  node.setStyle('color', 'red');
+  const matchedCSS = cssMap.query(node);
+  t.is(matchedCSS.selectors.length, 4);
+});
