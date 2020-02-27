adaca3940518738c2a24bfba3098f9b5e8f8c8e3
XQ Kuang <x@kxq.io>
2020-02-27 17:49:33 +0800

    refactor(hippy-vue-native-components): dynamic event binding

---
+++ packages/hippy-vue-native-components/src/utils.js
@@ -0,0 +1,41 @@
+/**
+ * Capitalize a word
+ *
+ * @param {string} s The word input
+ * @returns string
+ */
+function capitalize(str) {
+  if (typeof str !== 'string') {
+    return '';
+  }
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+}
+
+/**
+ * Get binding events redirector
+ *
+ * The function should be calld with `getEventRedirector.call(this, [])`
+ * for binding this.
+ *
+ * @param {string[] | string[][]} events events will be redirect
+ * @returns Object
+ */
+function getEventRedirector(events) {
+  const on = {};
+  events.forEach((event) => {
+    if (Array.isArray(event)) {
+      const [exposedEventName, nativeEventName] = event;
+      if (Object.prototype.hasOwnProperty.call(this.$listeners, exposedEventName)) {
+        on[event] = this[`on${capitalize(nativeEventName)}`];
+      }
+    } else if (Object.prototype.hasOwnProperty.call(this.$listeners, event)) {
+      on[event] = this[`on${capitalize(event)}`];
+    }
+  });
+  return on;
+}
+
+export {
+  capitalize,
+  getEventRedirector,
+};
