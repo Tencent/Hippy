e8ce151e1cd91c8a8e42ee0a0e4479fc817d7822
maxli <maxli@tencent.com>
2020-07-28 16:58:20 +0800

    fix(android-QB): support-ui depend from qb mirrors

--- android/sdk/src/main/java/com/tencent/mtt/supportui/views/recyclerview/IBlockTouchListener.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.mtt.supportui.views.recyclerview;
-
-/**
- * Created by niuniuyang on 2020-03-06.
- * Description
- */
-public interface IBlockTouchListener {
-    void onRecyclerViewTouchEnabled(boolean enableTouch);
-}
