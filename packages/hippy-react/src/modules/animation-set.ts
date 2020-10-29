6e9ffcea8245f01bedf43aeb64bba256973278a5
Zoom Chan <zoom_chan@163.com>
2020-10-29 16:19:13 +0800

    fix(hippy-react): fix hippy-react animationSet destroy problem (#382)
    Co-authored-by: shermanchen <shermanchen@tencent.com>

--- packages/hippy-react/src/modules/animation-set.ts
+++ packages/hippy-react/src/modules/animation-set.ts
@@ -157,6 +157,8 @@ class AnimationSet implements AnimationSet {
    */
   public destroy() {
     this.removeEventListener();
+    this.animationList.forEach(item => item.animationId
+        && Bridge.callNative('AnimationModule', 'destroyAnimation', item.animationId));
     Bridge.callNative('AnimationModule', 'destroyAnimation', this.animationId);
   }
 
