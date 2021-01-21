4192fe3cdcfaed0965f92be1ac11fe7d1a03a374
zoomchan-cxj <zoom_chan@163.com>
2021-01-21 15:50:34 +0800

    chore(doc): add dymamic load compatible info

--- examples/hippy-react-demo/src/externals/DyanmicImport/index.jsx
+++ examples/hippy-react-demo/src/externals/DyanmicImport/index.jsx
@@ -30,6 +30,8 @@ export default class DynamicImportDemo extends React.Component {
   onAsyncComponentLoad() {
     /* eslint-disable-next-line no-console */
     console.log('load async component');
+    // 在已经支持动态加载的终端版本，可以加 /* webpackMode: "lazy" */ 注释也可以不加，默认就是lazy模式；
+    // 在不支持动态加载的终端版本，可以添加 /* webpackMode: "eager" */ 不进行分包
     import(/* webpackMode: "lazy" */ './AsyncComponent').then((component) => {
       this.setState({
         AsyncComponent: component.default ? component.default : component,
