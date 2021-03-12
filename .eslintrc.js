bae449aefcd6efe393f23608f70f32a92b8a07bd
zoomchan-cxj <zoom_chan@163.com>
2021-03-12 10:36:41 +0800

    refactor(hippy-vue,hippy-react): change eslint to eslint-config-tencent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    '@tencent/eslint-config-tencent',
+    'eslint-config-tencent',
     'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
@@ -31,6 +31,60 @@ module.exports = {
     Hippy: true,
   },
   rules: {
+    'no-restricted-syntax': [
+      'warn',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'no-mixed-operators': ['error', {
+      groups: [
+        ['%', '**'],
+        ['%', '+'],
+        ['%', '-'],
+        ['%', '*'],
+        ['%', '/'],
+        ['&', '|', '<<', '>>', '>>>'],
+        ['==', '!=', '===', '!=='],
+        ['&&', '||'],
+      ],
+      allowSamePrecedence: false
+    }],
+    'func-call-spacing': 'off',
+    'new-cap': [
+      'error',
+      {
+        newIsCap: true,
+        newIsCapExceptions: [],
+        capIsNew: false,
+        capIsNewExceptions: ['Immutable.Map', 'Immutable.Set', 'Immutable.List'],
+        properties: false,
+      }
+    ],
+    'prefer-destructuring': [
+      'warn',
+      {   VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: true,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
     quotes: [
       'error',
       'single',
