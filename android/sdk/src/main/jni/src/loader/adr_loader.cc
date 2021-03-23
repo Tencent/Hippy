4a40a14dc26e2807ee9a9c1b607ab5ef93a588ba
pollyzhang <pollyzhang@tencent.com>
2021-03-23 11:20:03 +0800

    feat(core): support multi scheme dynamic load

--- android/sdk/src/main/jni/src/loader/adr_loader.cc
+++ android/sdk/src/main/jni/src/loader/adr_loader.cc
@@ -22,17 +22,163 @@
 
 #include "loader/adr_loader.h"
 
+#include <future>
+
 #include "core/core.h"
+#include "jni/jni_env.h"
+#include "jni/jni_utils.h"
 #include "jni/uri.h"
 
+static std::atomic<int64_t> global_request_id{0};
 
-ADRLoader::ADRLoader() {}
-ADRLoader::ADRLoader(const std::string& base) : base_(base) {}
-
-std::string ADRLoader::Normalize(const std::string& uri) {
-  std::shared_ptr<Uri> uri_obj = std::make_shared<Uri>(uri);
-  if (uri_obj->GetScheme().empty()) {
-    return std::make_shared<Uri>(base_ + uri)->Normalize();
+static std::string ReadAsset(const std::string& file_path,
+                             AAssetManager* aasset_manager,
+                             bool is_auto_fill) {
+  std::string path(file_path);
+  if (path.length() > 0 && path[0] == '/') {
+    path = path.substr(1);
   }
-  return uri_obj->Normalize();
+  HIPPY_LOG(hippy::Debug, "path = %s", path.c_str());
+  auto asset =
+      AAssetManager_open(aasset_manager, path.c_str(), AASSET_MODE_STREAMING);
+  std::string file_data;
+  if (asset) {
+    int size = AAsset_getLength(asset);
+    if (is_auto_fill) {
+      size += 1;
+    }
+    file_data.resize(size);
+    int offset = 0;
+    int readbytes;
+    while ((readbytes = AAsset_read(asset, &file_data[0] + offset,
+                                    file_data.size() - offset)) > 0) {
+      offset += readbytes;
+    }
+    if (is_auto_fill) {
+      file_data.back() = '\0';
+    }
+    AAsset_close(asset);
+  }
+  HIPPY_DLOG(hippy::Debug, "file_path = %s, len = %d,  file_data = %s",
+             file_path.c_str(), file_data.length(), file_data.c_str());
+  return file_data;
+}
+
+ADRLoader::ADRLoader() : aasset_manager_(nullptr) {}
+
+bool ADRLoader::RequestUntrustedContent(const std::string& uri,
+                                        std::function<void(std::string)> cb) {
+  std::shared_ptr<Uri> uri_obj = std::make_shared<Uri>(uri);
+  std::string uri_schema = uri_obj->GetScheme();
+  std::string path = uri_obj->GetPath();
+  if (uri_schema == "file") {
+    return LoadByFile(path, cb);
+  } else if (uri_schema == "http" || uri_schema == "https" ||
+             uri_schema == "debug") {
+    return LoadByHttp(uri, cb);
+  } else if (uri_schema == "asset") {
+    if (aasset_manager_) {
+      return LoadByAsset(path, cb, false);
+    }
+
+    HIPPY_LOG(hippy::Error, "aasset_manager error, uri = %s", uri.c_str());
+    return false;
+  } else {
+    HIPPY_LOG(hippy::Error, "schema error, schema = %s", uri_schema.c_str());
+    return false;
+  }
+}
+
+std::string ADRLoader::RequestUntrustedContent(const std::string& uri) {
+  std::shared_ptr<Uri> uri_obj = std::make_shared<Uri>(uri);
+  std::string uri_schema = uri_obj->GetScheme();
+  std::string path = uri_obj->GetPath();
+  if (uri_schema == "file") {
+    return hippy::base::HippyFile::ReadFile(path.c_str(), false);
+  } else if (uri_schema == "http" || uri_schema == "https" ||
+             uri_schema == "debug") {
+    std::promise<std::string> promise;
+    std::future<std::string> read_file_future = promise.get_future();
+    std::function<void(std::string)> cb =
+        hippy::base::MakeCopyable([p = std::move(promise)](std::string content) mutable {
+          p.set_value(std::move(content));
+    });
+    LoadByHttp(uri, cb);
+    return read_file_future.get();
+  } else if (uri_schema == "asset") {
+    if (aasset_manager_) {
+      return ReadAsset(path, aasset_manager_, false);
+    }
+
+    HIPPY_LOG(hippy::Error, "aasset_manager error, uri = %s", uri.c_str());
+    return "";
+  } else {
+    HIPPY_LOG(hippy::Error, "schema error, schema = %s", uri_schema.c_str());
+    return "";
+  }
+}
+
+bool ADRLoader::LoadByFile(const std::string& path,
+                           std::function<void(std::string)> cb) {
+  std::shared_ptr<WorkerTaskRunner> runner = runner_.lock();
+  if (!runner) {
+    return false;
+  }
+  std::unique_ptr<CommonTask> task = std::make_unique<CommonTask>();
+  task->func_ = [path, cb] {
+    std::string ret = hippy::base::HippyFile::ReadFile(path.c_str(), false);
+    cb(std::move(ret));
+  };
+  runner->PostTask(std::move(task));
+
+  return true;
+}
+
+bool ADRLoader::LoadByAsset(const std::string& file_path,
+                            std::function<void(std::string)> cb,
+                            bool is_auto_fill) {
+  HIPPY_LOG(hippy::Debug, "ReadAssetFile file_path = %s", file_path.c_str());
+
+  std::shared_ptr<WorkerTaskRunner> runner = runner_.lock();
+  if (!runner) {
+    return false;
+  }
+  std::unique_ptr<CommonTask> task = std::make_unique<CommonTask>();
+  task->func_ = [file_path, aasset_manager = aasset_manager_, is_auto_fill,
+                 cb] {
+    std::string ret = ReadAsset(file_path, aasset_manager, is_auto_fill);
+    cb(std::move(ret));
+  };
+  runner->PostTask(std::move(task));
+
+  return true;
+}
+
+bool ADRLoader::LoadByHttp(const std::string& uri,
+                           std::function<void(std::string)> cb) {
+  JNIEnv* env = JNIEnvironment::AttachCurrentThread();
+  JNIEnvironment* instance = JNIEnvironment::GetInstance();
+  if (instance->wrapper_.fetch_resource_method_id) {
+    int64_t id = SetRequestCB(cb);
+    jstring j_relative_path = env->NewStringUTF(uri.c_str());
+    env->CallObjectMethod(bridge_->GetObj(),
+                          instance->wrapper_.fetch_resource_method_id,
+                          j_relative_path, id);
+    env->DeleteLocalRef(j_relative_path);
+    return true;
+  }
+
+  HIPPY_LOG(hippy::Error, "jni fetch_resource_method_id error");
+  return false;
+}
+
+std::function<void(std::string)> ADRLoader::GetRequestCB(int64_t request_id) {
+  auto it = request_map_.find(request_id);
+  return it != request_map_.end() ? it->second : nullptr;
+}
+
+int64_t ADRLoader::SetRequestCB(std::function<void(std::string)> cb) {
+  int64_t id = global_request_id.fetch_add(1);
+  request_map_.insert({id, cb});
+  return id;
 }
