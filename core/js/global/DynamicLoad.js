32c5fd7a89c7b8c17780077390adc7fed0693b03
zoomchan-cxj <zoom_chan@163.com>
2021-03-24 15:20:47 +0800

    refactor(core): perf dynamic load code

--- core/js/global/DynamicLoad.js
+++ core/js/global/DynamicLoad.js
@@ -1,12 +1,10 @@
 const ContextifyModule = internalBinding('ContextifyModule');
 
 global.dynamicLoad = (path, encode, cb) => {
-  let requestPath = path;
-  const reg = /^(https?:)?\/\/.+$/;
-  if (path && !reg.test(path)) {
+  let requestPath = path || '';
+  const isSchema = ['https://', 'http://', '//'].some(schema => requestPath.indexOf(schema) === 0);
+  if (!isSchema) {
     requestPath = global.__HIPPYCURDIR__ + path;
-    console.log(`global.__HIPPYCURDIR__: ${global.__HIPPYCURDIR__}`);
   }
-  console.log(`requestPath:${requestPath}|path = ${path}`);
   ContextifyModule.LoadUntrustedContent(requestPath, encode, cb);
 };
