f6255c43b6df36007220fd7ce508e02ffde3daa6
pollyzhang <pollyzhang@tencent.com>
2021-04-02 16:57:39 +0800

    feat(core): add dynamic jni

--- android/sdk/src/main/jni/include/jni/register.h
+++ android/sdk/src/main/jni/include/jni/register.h
@@ -10,7 +10,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE_2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,13 +22,8 @@
 
 #pragma once
 
-#include "inspector/v8_channel_impl.h"
-#include "inspector/v8_inspector_client_impl.h"
-#include "jni/exception_handler.h"
-#include "jni/jni_env.h"
-#include "jni/jni_utils.h"
-#include "jni/register.h"
-#include "jni/runtime.h"
-#include "jni/scoped_java_ref.h"
-#include "loader/adr_loader.h"
-#include "v8/v8.h"
+#define JNI_NATIVE_METHOD(name, signature, pointer)          \
+  {                                                          \
+    const_cast<char *>(name), const_cast<char *>(signature), \
+        reinterpret_cast<void *>(pointer)                    \
+  }
