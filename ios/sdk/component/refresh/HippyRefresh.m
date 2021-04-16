7b7746b55549466d51f1ce9aeae2ae63efa87235
mengyanluo <mengyanluo@tencent.com>
2021-04-16 17:25:44 +0800

    feat(ios): pull header feature to set refresh time

--- ios/sdk/component/refresh/HippyRefresh.m
+++ ios/sdk/component/refresh/HippyRefresh.m
@@ -54,10 +54,7 @@
 }
 
 - (void)refreshFinish {
-    self.status = HippyRefreshStatusFinishLoading;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.status = HippyRefreshStatusIdle;
-    });
+    [self refreshFinishWithOption:@{@"time": @(2000)}];
 }
 
 - (void)refreshFinishWithOption:(NSDictionary *)options {
