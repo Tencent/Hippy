a119e08fbe40c71129fab190d905c65a485c5b1f
maxli <maxli@tencent.com>
2021-04-22 21:52:00 +0800

    feat(android): support set custom C log handler

--- android/sdk/src/main/java/com/tencent/mtt/hippy/bridge/HippyLogger.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.mtt.hippy.bridge;
-
-import com.tencent.mtt.hippy.utils.LogUtils;
-
-public class HippyLogger {
-  void Call(String str) {
-    LogUtils.e("HippyCore", str);
-  }
-}
