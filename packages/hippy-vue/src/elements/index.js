52e44f7662ab8edb164ac5e95ae1c1bed8b70bd3
zoomchan-cxj <zoom_chan@163.com>
2021-04-25 18:09:40 +0800

    fix(hippy-vue): fix registerElement elem name and comp name problem
    Cannot registerElement with kebab-case name  which converted to camelCase is the same with component.name

--- packages/hippy-vue/src/elements/index.js
+++ packages/hippy-vue/src/elements/index.js
@@ -1,5 +1,6 @@
-import { makeMap } from 'shared/util';
+import { makeMap, camelize } from 'shared/util';
 import * as BUILT_IN_ELEMENTS from './built-in';
+import { capitalizeFirstLetter } from '../util';
 
 const isReservedTag = makeMap(
   'template,script,style,element,content,slot,'
@@ -36,6 +37,9 @@ function normalizeElementName(elementName) {
 }
 
 function registerElement(elementName, oldMeta) {
+  if (!elementName) {
+    throw new Error('RegisterElement cannot set empty name');
+  }
   const normalizedName = normalizeElementName(elementName);
 
   const meta = { ...defaultViewMeta, ...oldMeta };
@@ -48,6 +52,10 @@ function registerElement(elementName, oldMeta) {
     ...getDefaultComponent(elementName, meta, normalizedName),
     ...meta.component,
   };
+
+  if (meta.component.name && meta.component.name === capitalizeFirstLetter(camelize(elementName))) {
+    throw new Error(`Cannot registerElement with kebab-case name ${elementName}, which converted to camelCase is the same with component.name ${meta.component.name}, please make them different`);
+  }
 
   const entry = {
     meta,
