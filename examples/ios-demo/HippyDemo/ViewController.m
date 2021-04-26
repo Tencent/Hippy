0499d7bb5a96525c7632a6a11d2e4d951969c92e
mengyanluo <mengyanluo@tencent.com>
2021-04-26 15:32:39 +0800

    Revert "feat(ios): add log handler"
    This reverts commit f6ea029087f699895fd2fe0f4d0e0de7bc873de6.

--- examples/ios-demo/HippyDemo/ViewController.m
+++ examples/ios-demo/HippyDemo/ViewController.m
@@ -22,7 +22,6 @@
 
 #import "ViewController.h"
 #import "HippyRootView.h"
-#import "HippyLog.h"
 
 @interface ViewController ()<HippyBridgeDelegate>
 
@@ -33,11 +32,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    logHandler handler = ^(const char *message) {
-        NSLog(@"hippy core says:%s", message);
-    };
-    HippyLogSetHandler(handler);
 
     BOOL isSimulator = NO;
     #if TARGET_IPHONE_SIMULATOR
@@ -57,7 +51,6 @@
     rootView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     rootView.frame = self.view.bounds;
     [self.view addSubview:rootView];
-    
 }
 
 - (NSDictionary *)objectsBeforeExecuteCode {
--- ios/sdk/utils/HippyLog.mm
+++ ios/sdk/utils/HippyLog.mm
@@ -30,7 +30,6 @@
 #import "HippyDefines.h"
 #import "HippyRedBox.h"
 #import "HippyUtils.h"
-#import "logging.h"
 
 static NSString *const HippyLogFunctionStack = @"HippyLogFunctionStack";
 
@@ -235,11 +234,4 @@ void _HippyLogJavaScriptInternal(HippyLogLevel level, NSString *message) {
             logFunction(level, HippyLogSourceJavaScript, nil, nil, message);
         }
     }
-}
-
-void HippyLogSetHandler(logHandler handler) {
-    std::function<void (const std::ostringstream &)> logFunction = [handler](const std::ostringstream &stream) {
-        handler(stream.str().c_str());
-    };
-    tdf::base::LogMessage::SetDefaultDelegate(logFunction);
 }
