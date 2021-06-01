37944ca78aa477dfc9e4097c9087d176bd72148e
pollyzhang <pollyzhang@tencent.com>
2021-06-01 21:37:45 +0800

    fix(core): fix unicode_string_view null pointer bug

--- core/src/base/file.cc
+++ core/src/base/file.cc
@@ -31,20 +31,17 @@
 #include <vector>
 
 #include "base/logging.h"
-#include "base/unicode_string_view.h"
-#include "core/base/string_view_utils.h"
 
 namespace hippy {
 namespace base {
 
-using unicode_string_view = tdf::base::unicode_string_view;
-using StringViewUtils = hippy::base::StringViewUtils;
-
-bool HippyFile::SaveFile(const char* file_path,
+bool HippyFile::SaveFile(const unicode_string_view& file_path,
                          const std::string& content,
                          std::ios::openmode mode) {
   TDF_BASE_DLOG(INFO) << "SaveFile file_path = " << file_path;
-  std::ofstream file(file_path, mode);
+  unicode_string_view owner(""_u8s);
+  const char* path = StringViewUtils::ToConstCharPointer(file_path, owner);
+  std::ofstream file(path, mode);
   if (file.is_open()) {
     file.write(content.c_str(), content.length());
     file.close();
@@ -54,9 +51,11 @@ bool HippyFile::SaveFile(const char* file_path,
   }
 }
 
-int HippyFile::RmFullPath(std::string dir_full_path) {
+int HippyFile::RmFullPath(const unicode_string_view& dir_full_path) {
   TDF_BASE_DLOG(INFO) << "RmFullPath dir_full_path = " << dir_full_path;
-  DIR* dir_parent = opendir(dir_full_path.c_str());
+  unicode_string_view owner(""_u8s);
+  const char* path = StringViewUtils::ToConstCharPointer(dir_full_path, owner);
+  DIR* dir_parent = opendir(path);
   if (!dir_parent) {
     TDF_BASE_DLOG(INFO) << "RmFullPath dir_parent null";
     return -1;
@@ -67,13 +66,16 @@ int HippyFile::RmFullPath(std::string dir_full_path) {
     if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
       continue;
     }
-    std::string sub_path = dir_full_path + '/' + dir->d_name;
+    std::string sub_path = std::string(path) + '/' + std::string(dir->d_name);
+    unicode_string_view view_sub_path =
+        StringViewUtils::ConstCharPointerToStrView(sub_path.c_str(),
+                                                   sub_path.length());
     TDF_BASE_DLOG(INFO) << "RmFullPath sub_path = " << sub_path;
     if (lstat(sub_path.c_str(), &st) == -1) {
       continue;
     }
     if (S_ISDIR(st.st_mode)) {
-      if (RmFullPath(sub_path) == -1) {  // 如果是目录文件，递归删除
+      if (RmFullPath(view_sub_path) == -1) {  // 如果是目录文件，递归删除
         return -1;
       }
       rmdir(sub_path.c_str());
@@ -83,7 +85,7 @@ int HippyFile::RmFullPath(std::string dir_full_path) {
       continue;
     }
   }
-  if (rmdir(dir_full_path.c_str()) == -1) {
+  if (rmdir(path) == -1) {
     TDF_BASE_DLOG(INFO) << "RmFullPath delete dir_full_path fail, path = "
                         << dir_full_path;
     closedir(dir_parent);
@@ -94,21 +96,26 @@ int HippyFile::RmFullPath(std::string dir_full_path) {
   return 0;
 }
 
-int HippyFile::CreateDir(const char* path, mode_t mode) {
+int HippyFile::CreateDir(const unicode_string_view& path, mode_t mode) {
   TDF_BASE_DLOG(INFO) << "CreateDir path = " << path;
-  return mkdir(path, mode);
+  unicode_string_view owner(""_u8s);
+  const char* dir_path = StringViewUtils::ToConstCharPointer(path, owner);
+  return mkdir(dir_path, mode);
 }
 
-int HippyFile::CheckDir(const char* path, int mode) {
+int HippyFile::CheckDir(const unicode_string_view& path, int mode) {
   TDF_BASE_DLOG(INFO) << "CheckDir path = " << path;
-  return access(path, mode);
+  unicode_string_view owner(""_u8s);
+  const char* dir_path = StringViewUtils::ToConstCharPointer(path, owner);
+  return access(dir_path, mode);
 }
 
-uint64_t HippyFile::GetFileModifytime(const char* file_path) {
+uint64_t HippyFile::GetFileModifytime(const unicode_string_view& file_path) {
   TDF_BASE_DLOG(INFO) << "GetFileModifytime file_path = " << file_path;
+  unicode_string_view view_owner(""_u8s);
+  const char* path = StringViewUtils::ToConstCharPointer(file_path, view_owner);
   struct stat statInfo;
-
-  FILE* fp = fopen(file_path, "r");
+  FILE* fp = fopen(path, "r");
   if (fp == nullptr) {
     return 0;
   }
