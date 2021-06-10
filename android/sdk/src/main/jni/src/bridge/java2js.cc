cdf2c11c86e5cf5acc6c00bbf312ef9fcbd40f93
pollyzhang <pollyzhang@tencent.com>
2021-06-10 15:59:18 +0800

    fix(core): fix jni crash

--- android/sdk/src/main/jni/src/bridge/java2js.cc
+++ android/sdk/src/main/jni/src/bridge/java2js.cc
@@ -79,9 +79,10 @@ void CallFunction(JNIEnv* j_env,
   unicode_string_view action_name = JniUtils::ToStrView(j_env, j_action);
   std::shared_ptr<JavaRef> cb = std::make_shared<JavaRef>(j_env, j_callback);
   std::shared_ptr<JavaScriptTask> task = std::make_shared<JavaScriptTask>();
-  task->callback = [j_env, runtime, cb_ = std::move(cb), action_name,
+  task->callback = [runtime, cb_ = std::move(cb), action_name,
                     buffer_data_ = std::move(buffer_data),
                     buffer_owner_ = std::move(buffer_owner)] {
+    JNIEnv* j_env = JNIEnvironment::GetInstance()->AttachCurrentThread();
     std::shared_ptr<Scope> scope = runtime->GetScope();
     if (!scope) {
       TDF_BASE_DLOG(WARNING) << "CallFunction scope invalid";
@@ -94,10 +95,13 @@ void CallFunction(JNIEnv* j_env,
       std::shared_ptr<CtxValue> fn = context->GetJsFn(name);
       bool is_fn = context->IsFunction(fn);
       TDF_BASE_DLOG(INFO) << "is_fn = " << is_fn;
+
       if (!is_fn) {
-        CallJavaMethod(
-            cb_->GetObj(), CALLFUNCTION_CB_STATE::NO_METHOD_ERROR,
-            JniUtils::StrViewToJString(j_env, u"hippyBridge not find"));
+        jstring j_msg =
+            JniUtils::StrViewToJString(j_env, u"hippyBridge not find");
+        CallJavaMethod(cb_->GetObj(), CALLFUNCTION_CB_STATE::NO_METHOD_ERROR,
+                       j_msg);
+        j_env->DeleteLocalRef(j_msg);
         return;
       } else {
         runtime->SetBridgeFunc(fn);
@@ -135,17 +139,18 @@ void CallFunction(JNIEnv* j_env,
         params = std::make_shared<hippy::napi::V8CtxValue>(
             isolate, ret.ToLocalChecked());
       } else {
+        jstring j_msg =
+            JniUtils::StrViewToJString(j_env, u"deserializer error");
         CallJavaMethod(
             cb_->GetObj(),
-            hippy::bridge::CALLFUNCTION_CB_STATE::DESERIALIZER_FAILED,
-            JniUtils::StrViewToJString(j_env, u"deserializer error"));
+            hippy::bridge::CALLFUNCTION_CB_STATE::DESERIALIZER_FAILED, j_msg);
+        j_env->DeleteLocalRef(j_msg);
         return;
       }
     } else {
       std::u16string str(reinterpret_cast<const char16_t*>(&buffer_data_[0]),
                          buffer_data_.length() / sizeof(char16_t));
-      unicode_string_view buf_str =
-          std::move(unicode_string_view(std::move(str)));
+      unicode_string_view buf_str(std::move(str));
       TDF_BASE_DLOG(INFO) << "action_name = " << action_name
                           << ", buf_str = " << buf_str;
       params = context->CreateObject(buf_str);
@@ -190,23 +195,22 @@ void CallFunctionByDirectBuffer(JNIEnv* j_env,
 }
 
 void CallJavaMethod(jobject j_obj, jlong j_value, jstring j_msg) {
-  jclass j_class = nullptr;
   if (!j_obj) {
     TDF_BASE_DLOG(INFO) << "CallJavaMethod j_obj is nullptr";
     return;
   }
 
   JNIEnv* j_env = JNIEnvironment::GetInstance()->AttachCurrentThread();
-  j_class = j_env->GetObjectClass(j_obj);
+  jclass j_class = j_env->GetObjectClass(j_obj);
   if (!j_class) {
-    TDF_BASE_DLOG(ERROR) << "CallJavaMethod j_class error";
+    TDF_BASE_LOG(ERROR) << "CallJavaMethod j_class error";
     return;
   }
 
   jmethodID j_cb_id =
       j_env->GetMethodID(j_class, "Callback", "(JLjava/lang/String;)V");
   if (!j_cb_id) {
-    TDF_BASE_DLOG(ERROR) << "CallJavaMethod j_cb_id error";
+    TDF_BASE_LOG(ERROR) << "CallJavaMethod j_cb_id error";
     return;
   }
 
