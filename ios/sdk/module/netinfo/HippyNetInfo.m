5cee675ce3802863e085b2247ff42fad3c38f95a
mengyanluo <mengyanluo@tencent.com>
2021-06-11 18:01:15 +0800

    feat(ios): users can get cell type from hippynetinfo module

--- ios/sdk/module/netinfo/HippyNetInfo.m
+++ ios/sdk/module/netinfo/HippyNetInfo.m
@@ -25,108 +25,49 @@
 #import "HippyAssert.h"
 #import "HippyBridge.h"
 #import "HippyEventDispatcher.h"
-#import "netinet/in.h"
+#import "HippyNetInfoIntenal.h"
 
-static NSString *const HippyReachabilityStateUnknown = @"UNKNOWN";
-static NSString *const HippyReachabilityStateNone = @"NONE";
-static NSString *const HippyReachabilityStateWifi = @"WIFI";
-static NSString *const HippyReachabilityStateCell = @"CELL";
+@interface HippyNetInfo ()<HippyNetworkTypeChangedDelegate> {
+}
 
-@implementation HippyNetInfo {
-    SCNetworkReachabilityRef _reachability;
-    NSString *_networkType;
-    NSString *_host;
+@end
+
+@implementation HippyNetInfo
+
+static NSDictionary *callbackParamFromCellType(HippyNetworkTypeObject *object) {
+    if (!object) {
+        return @{};
+    }
+    NSString *networkType = object.networkType?:HippyNetworkTypeUnknown;
+    NSString *cellType = object.cellType?:HippNetworkCellTypeUnknown;
+    return @{@"network_info": networkType, @"network_type":cellType};
 }
 
 HIPPY_EXPORT_MODULE()
 
-static NSString *hippyReachabilityTypeFromFlags(SCNetworkReachabilityFlags flags) {
-    NSString *networkType = HippyReachabilityStateUnknown;
-    if ((flags & kSCNetworkReachabilityFlagsReachable) == 0 || (flags & kSCNetworkReachabilityFlagsConnectionRequired) != 0) {
-        networkType = HippyReachabilityStateNone;
-    } else if ((flags & kSCNetworkReachabilityFlagsIsWWAN) != 0) {
-        networkType = HippyReachabilityStateCell;
-    } else {
-        networkType = HippyReachabilityStateWifi;
-    }
-    return networkType;
-}
-
-static NSString *currentReachabilityType(SCNetworkReachabilityRef reachabilityRef) {
-    SCNetworkReachabilityFlags flags;
-    BOOL success = SCNetworkReachabilityGetFlags(reachabilityRef, &flags);
-    if (success) {
-        NSString *type = hippyReachabilityTypeFromFlags(flags);
-        return type ?: HippyReachabilityStateUnknown;
-    } else {
-        return HippyReachabilityStateUnknown;
-    }
-}
-
-static SCNetworkReachabilityRef createReachabilityRefWithZeroAddress() {
-    struct sockaddr_in zeroAddress;
-    bzero(&zeroAddress, sizeof(zeroAddress));
-    zeroAddress.sin_len = sizeof(zeroAddress);
-    zeroAddress.sin_family = AF_INET;
-    SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (struct sockaddr *)&zeroAddress);
-    return reachability;
-}
-
-static void HippyReachabilityCallback(__unused SCNetworkReachabilityRef target, SCNetworkReachabilityFlags flags, void *info) {
-    HippyNetInfo *self = (__bridge id)info;
-    NSString *networkType = hippyReachabilityTypeFromFlags(flags);
-    if (![networkType isEqualToString:self->_networkType]) {
-        self->_networkType = networkType;
-        [self sendEvent:@"networkStatusDidChange" params:@ { @"network_info": networkType }];
-    }
-}
-
-#pragma mark - Lifecycle
-
-- (instancetype)initWithHost:(NSString *)host {
-    HippyAssertParam(host);
-    HippyAssert(![host hasPrefix:@"http"], @"Host value should just contain the domain, not the URL scheme.");
-
-    if ((self = [self init])) {
-        _host = [host copy];
-    }
-    return self;
-}
-
 - (void)addEventObserverForName:(NSString *)eventName {
     if ([eventName isEqualToString:@"networkStatusDidChange"]) {
-        if (!_reachability) {
-            _reachability = createReachabilityRefWithZeroAddress();
-            SCNetworkReachabilityContext context = { 0, (__bridge void *)self, NULL, NULL, NULL };
-            SCNetworkReachabilitySetCallback(_reachability, HippyReachabilityCallback, &context);
-            SCNetworkReachabilityScheduleWithRunLoop(_reachability, CFRunLoopGetMain(), kCFRunLoopCommonModes);
-        }
-        _networkType = currentReachabilityType(_reachability);
-        [self sendEvent:@"networkStatusDidChange" params:@{ @"network_info": _networkType }];
+        HippyNetworkTypeObject *networkType = [[HippyNetInfoIntenal sharedInstance] addNetworkTypeChangeObserver:self];
+        NSDictionary *params = callbackParamFromCellType(networkType);
+        [self sendEvent:@"networkStatusDidChange" params:params];
+
     }
 }
 
 - (void)removeEventObserverForName:(NSString *)eventName {
     if ([eventName isEqualToString:@"networkStatusDidChange"]) {
-        [self releaseReachability];
+        [[HippyNetInfoIntenal sharedInstance] removeNetworkTypeChangeObserver:self];
     }
 }
 
 - (void)invalidate {
-    [self releaseReachability];
 }
 
-- (void)releaseReachability {
-    if (_reachability) {
-        SCNetworkReachabilityUnscheduleFromRunLoop(_reachability, CFRunLoopGetMain(), kCFRunLoopCommonModes);
-        CFRelease(_reachability);
-        _reachability = NULL;
-    }
+- (void)hippyNetworkTypeChanged:(HippyNetworkTypeObject *)networkType {
+    NSDictionary *params = callbackParamFromCellType(networkType);
+    [self sendEvent:@"networkStatusDidChange" params:params];
 }
 
-- (void)dealloc {
-    [self releaseReachability];
-}
 #pragma mark - Public API
 
 // clang-format off
@@ -135,23 +76,9 @@ HIPPY_EXPORT_METHOD(getCurrentConnectivity:(HippyPromiseResolveBlock)resolve
     if (!resolve) {
         return;
     }
-    //return network type if it was set and not unknown type
-    if (_networkType) {
-        resolve(@{@"network_info": _networkType});
-    }
-    //else try to get network type
-    else {
-        SCNetworkReachabilityRef reachability = NULL;
-        if (_reachability) {
-            reachability = CFRetain(_reachability);
-        }
-        else {
-            reachability = createReachabilityRefWithZeroAddress();
-        }
-        NSString *type = currentReachabilityType(reachability);
-        resolve(@{@"network_info": type});
-        CFRelease(reachability);
-    }
+    HippyNetworkTypeObject *obj = [[HippyNetInfoIntenal sharedInstance] currentNetworkType];
+    NSDictionary *dic = callbackParamFromCellType(obj);
+    resolve(dic);
 }
 // clang-format on
 
