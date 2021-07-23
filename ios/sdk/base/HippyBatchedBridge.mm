1f38c66ec4f4ef910050ca28dd7f2455307fdf92
mengyanluo <mengyanluo@tencent.com>
2021-07-23 12:10:59 +0800

    feat(ios): modify localization infomations

--- ios/sdk/base/HippyBatchedBridge.mm
+++ ios/sdk/base/HippyBatchedBridge.mm
@@ -586,16 +586,11 @@ HIPPY_NOT_IMPLEMENTED(-(instancetype)initWithDelegate
     if (_dimDic) {
         [deviceInfo setValue:_dimDic forKey:@"Dimensions"];
     }
-    if (@available(iOS 10.0, *)) {
-        NSString *countryCode = [[NSLocale currentLocale] countryCode];
-
-        NSString *lanCode = [[NSLocale currentLocale] languageCode];
-        
-        NSWritingDirection direction = [NSParagraphStyle defaultWritingDirectionForLanguage:lanCode];
-        
-        NSDictionary *local = @{@"countryCode": countryCode?:@"unknow", @"appLanguageCode": lanCode?:@"unknown", @"writingDirection": @(direction)};
-        [deviceInfo setValue:local forKey:@"Localization"];
-    }
+    NSString *countryCode = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+    NSString *lanCode = [[NSLocale currentLocale] objectForKey:NSLocaleLanguageCode];
+    NSWritingDirection direction = [NSParagraphStyle defaultWritingDirectionForLanguage:lanCode];
+    NSDictionary *local = @{@"country": countryCode?:@"unknown", @"language": lanCode?:@"unknown", @"direction": @(direction)};
+    [deviceInfo setValue:local forKey:@"Localization"];
     return [NSDictionary dictionaryWithDictionary:deviceInfo];
 }
 
