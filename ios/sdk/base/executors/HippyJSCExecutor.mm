e763bf3a79bbf243c72729440c95d55d2943886a
mengyanluo <mengyanluo@tencent.com>
2021-07-30 15:24:47 +0800

    feat(ios): add try catch block for js execution

--- ios/sdk/base/executors/HippyJSCExecutor.mm
+++ ios/sdk/base/executors/HippyJSCExecutor.mm
@@ -506,10 +506,7 @@ HIPPY_EXPORT_METHOD(setContextName:(NSString *)contextName) {
             if (!strongSelf || !strongSelf.isValid || nullptr == strongSelf.pScope) {
                 return;
             }
-
-#ifndef HIPPY_DEBUG
             @try {
-#endif
                 HippyBridge *bridge = [strongSelf bridge];
                 NSString *moduleName = [bridge moduleName];
                 NSError *executeError = nil;
@@ -562,11 +559,14 @@ HIPPY_EXPORT_METHOD(setContextName:(NSString *)contextName) {
                     objcValue = unwrapResult ? [objc_value toObject] : objc_value;
                 }
                 onComplete(objcValue, executeError);
-#ifndef HIPPY_DEBUG
             } @catch (NSException *exception) {
-                MttHippyException(exception);
+                NSString *moduleName = strongSelf.bridge.moduleName?:@"unknown";
+                NSMutableDictionary *userInfo = [exception.userInfo mutableCopy]?:[NSMutableDictionary dictionary];
+                [userInfo setObject:moduleName forKey:HippyFatalModuleName];
+                [userInfo setObject:arguments?:[NSArray array] forKey:@"arguments"];
+                NSException *reportException = [NSException exceptionWithName:exception.name reason:exception.reason userInfo:userInfo];
+                MttHippyException(reportException);
             }
-#endif
         }
     }];
 }
