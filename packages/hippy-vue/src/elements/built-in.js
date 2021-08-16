90a4267f4f96b581f7ee796cd6a362a648ba20fd
boxizeng <boxizeng@tencent.com>
2021-08-16 21:24:16 +0800

    feat(vue): support swipe to delete

--- packages/hippy-vue/src/elements/built-in.js
+++ packages/hippy-vue/src/elements/built-in.js
@@ -139,6 +139,9 @@ const ul = {
           event.offsetX = nativeEventParams.contentOffset.x;
           event.offsetY = nativeEventParams.contentOffset.y;
           break;
+        case 'onDelete': 
+          event.index = nativeEventParams.index
+          break;
         default:
       }
       return event;
