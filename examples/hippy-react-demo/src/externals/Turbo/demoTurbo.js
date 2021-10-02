368874dda6d3ef6ba7c65d693b0722132edb6aee
derekazhang <dami7786@qq.com>
2021-10-02 18:28:54 +0800

    feat(vue): add vue jsi

---
+++ examples/hippy-react-demo/src/externals/Turbo/demoTurbo.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle, no-undef */
+
+export const getString = strVal => getTurboModule('demoTurbo')
+  .getString(strVal);
+
+export const getNum = numVal => getTurboModule('demoTurbo')
+  .getNum(numVal);
+
+export const getBoolean = boolVal => getTurboModule('demoTurbo')
+  .getBoolean(boolVal);
+
+export const getMap = mapVal => getTurboModule('demoTurbo')
+  .getMap(mapVal);
+
+export const getObject = jsonVal => getTurboModule('demoTurbo')
+  .getObject(jsonVal);
+
+export const getArray = arrayVal => getTurboModule('demoTurbo')
+  .getArray(arrayVal);
+
+export const nativeWithPromise = async value => await turboPromise(getTurboModule('demoTurbo').nativeWithPromise)(value);
+
+export const getTurboConfig = () => getTurboModule('demoTurbo')
+  .getTurboConfig();
+
+export const printTurboConfig = config => getTurboModule('demoTurbo')
+  .printTurboConfig(config);
