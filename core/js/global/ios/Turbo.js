72b09f0534f6c9ed6d3cd60975f46a1f5a2df8e9
derekazhang <dami7786@qq.com>
2021-10-02 18:28:54 +0800

    feat(vue): jsi

---
+++ core/js/global/ios/Turbo.js
@@ -0,0 +1,19 @@
+function turboPromise(func) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      const failCallbackId = __GLOBAL__._callbackID;
+      __GLOBAL__._callbacks[failCallbackId] = (data) => {
+        resolve(data);
+      };
+      __GLOBAL__._callbackID += 1;
+      const successCallbackId = __GLOBAL__._callbackID;
+      __GLOBAL__._callbacks[successCallbackId] = (errorData) => {
+        reject(errorData);
+      };
+      __GLOBAL__._callbackID += 1;
+      func.apply(this, [...args, failCallbackId, successCallbackId]);
+    });
+  };
+}
+
+Hippy.turboPromise = turboPromise;
