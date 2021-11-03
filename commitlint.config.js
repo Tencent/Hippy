ba81d3067abea2c39a8d5a6cd109bad87e82dabd
zoomchan-cxj <zoom_chan@163.com>
2021-11-03 21:22:09 +0800

    ci(commitlint): forbid Chinese used in commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,30 @@
+function isIncludeChinese(message) {
+  const chineseReg = /[\u4e00-\u9fa5]+/i;
+  return chineseReg.test(message);
+}
+
 module.exports = {
   extends: [
     '@commitlint/config-conventional',
   ],
+  plugins: ['commitlint-plugin-function-rules'],
   rules: {
     'header-max-length': [2, 'always', 72],
+    'header-min-length': [0],
+    'function-rules/header-min-length': [
+      2,
+      'always',
+      (parsed) => {
+        const { subject } = parsed;
+        if (!subject) return [false, 'header subject cannot be empty'];
+        const { length } = subject;
+        const minLength = 8;
+        if (length >= minLength) {
+          return [true];
+        }
+        return [false, `header subject cannot be shorter than ${minLength} characters, current length is ${length}`];
+      },
+    ],
     'type-enum': [
       2,
       'always',
@@ -21,7 +42,19 @@ module.exports = {
         'test', // Adds or modifies tests.
       ],
     ],
-    'subject-case': [2, 'never', []],
     'scope-empty': [2, 'never'],
+    'subject-case': [0],
+    'function-rules/subject-case': [
+      2,
+      'always',
+      (parsed) => {
+        const { header, body, footer } = parsed;
+        const isContainChinese = [header, body, footer].some(message => isIncludeChinese(message));
+        if (isContainChinese) {
+          return [false, 'commit message cannot contain Chinese'];
+        }
+        return [true];
+      },
+    ],
   },
 };
