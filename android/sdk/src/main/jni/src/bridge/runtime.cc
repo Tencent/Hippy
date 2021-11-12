9c2c2798b1c3b5a70304678557a76da037afa6ab
churchill-zhang <pollyzhang@tencent.com>
2021-11-12 17:45:04 +0800

    fix(core): fix debug mode uncaught exception bug (#1058)
    * fix(core): fix debug mode uncaught exception bug
    
    * feat(core): v8 isolate custom data uses int32 to replace void*
    
    Co-authored-by: Zoom Chan <shermanchen@tencent.com>

--- android/sdk/src/main/jni/src/bridge/runtime.cc
+++ android/sdk/src/main/jni/src/bridge/runtime.cc
@@ -25,11 +25,12 @@
 #include <mutex>
 #include <unordered_map>
 
-static std::unordered_map<int64_t, std::shared_ptr<Runtime>> RuntimeMap;
-static std::unordered_map<int64_t, std::shared_ptr<int64_t>> RuntimeKeyMap;
+using V8Ctx = hippy::napi::V8Ctx;
+
+static std::unordered_map<int32_t, std::shared_ptr<Runtime>> RuntimeMap;
 static std::mutex mutex;
 
-static std::atomic<int64_t> global_runtime_key{0};
+static std::atomic<int32_t> global_runtime_key{0};
 
 Runtime::Runtime(std::shared_ptr<JavaRef> bridge, bool enable_v8_serialization, bool is_dev)
     : enable_v8_serialization_(enable_v8_serialization), is_debug_(is_dev), bridge_(bridge) {
@@ -38,12 +39,10 @@ Runtime::Runtime(std::shared_ptr<JavaRef> bridge, bool enable_v8_serialization, 
 
 void Runtime::Insert(std::shared_ptr<Runtime> runtime) {
   std::lock_guard<std::mutex> lock(mutex);
-  int64_t id = runtime->id_;
-  RuntimeKeyMap[id] = std::make_shared<int64_t>(id);
-  RuntimeMap[id] = runtime;
+  RuntimeMap[runtime->id_] = runtime;
 }
 
-std::shared_ptr<Runtime> Runtime::Find(int64_t id) {
+std::shared_ptr<Runtime> Runtime::Find(int32_t id) {
   std::lock_guard<std::mutex> lock(mutex);
   const auto it = RuntimeMap.find(id);
   if (it == RuntimeMap.end()) {
@@ -53,7 +52,30 @@ std::shared_ptr<Runtime> Runtime::Find(int64_t id) {
   return it->second;
 }
 
-bool Runtime::Erase(int64_t id) {
+static const uint32_t kRuntimeSlotIndex = 0;
+std::shared_ptr<Runtime> Runtime::Find(v8::Isolate *isolate) {
+  if (!isolate) {
+    return nullptr;
+  }
+  int32_t runtime_id =
+      static_cast<int32_t>(reinterpret_cast<int64_t>(isolate->GetData(kRuntimeSlotIndex)));
+  if (runtime_id == -1) {// -1 means single isolate multi context mode
+    v8::Local<v8::Context> context = isolate->GetCurrentContext();
+    std::lock_guard<std::mutex> lock(mutex);
+    for (auto p: RuntimeMap) {
+      std::shared_ptr<Scope> scope = p.second->GetScope();
+      std::shared_ptr<V8Ctx> ctx = std::static_pointer_cast<V8Ctx>(scope->GetContext());
+      if (ctx->context_persistent_ == context) {
+        return p.second;
+      }
+    }
+  } else {
+    return Runtime::Find(runtime_id);
+  }
+  return nullptr;
+}
+
+bool Runtime::Erase(int32_t id) {
   std::lock_guard<std::mutex> lock(mutex);
   const auto it = RuntimeMap.find(id);
   if (it == RuntimeMap.end()) {
@@ -66,25 +88,4 @@ bool Runtime::Erase(int64_t id) {
 
 bool Runtime::Erase(std::shared_ptr<Runtime> runtime) {
   return Runtime::Erase(runtime->id_);
-}
-
-std::shared_ptr<int64_t> Runtime::GetKey(std::shared_ptr<Runtime> runtime) {
-  std::lock_guard<std::mutex> lock(mutex);
-  const auto it = RuntimeKeyMap.find(runtime->id_);
-  if (it == RuntimeKeyMap.end()) {
-    return nullptr;
-  }
-
-  return it->second;
-}
-
-bool Runtime::ReleaseKey(int64_t id) {
-  std::lock_guard<std::mutex> lock(mutex);
-  const auto it = RuntimeKeyMap.find(id);
-  if (it == RuntimeKeyMap.end()) {
-    return false;
-  }
-
-  RuntimeKeyMap.erase(it);
-  return true;
 }
