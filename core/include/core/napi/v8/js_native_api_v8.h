5eaeaf50f453e3ab4e890143b7fcd1f0c0893dfb
pollyzhang <pollyzhang@tencent.com>
2021-11-23 17:28:13 +0800

    fix(core): fix lint warning

--- core/include/core/napi/v8/js_native_api_v8.h
+++ core/include/core/napi/v8/js_native_api_v8.h
@@ -128,69 +128,69 @@ class V8Ctx : public Ctx {
     global_persistent_.Empty();
   }
 
-  virtual bool RegisterGlobalInJs();
+  virtual bool RegisterGlobalInJs() override;
   virtual bool SetGlobalJsonVar(const unicode_string_view& name,
-                                const unicode_string_view& json);
+                                const unicode_string_view& json) override;
   virtual bool SetGlobalStrVar(const unicode_string_view& name,
-                               const unicode_string_view& str);
+                               const unicode_string_view& str) override;
   virtual bool SetGlobalObjVar(const unicode_string_view& name,
                                const std::shared_ptr<CtxValue>& obj,
-                               const PropertyAttribute& attr = None);
+                               const PropertyAttribute& attr = None) override;
   virtual std::shared_ptr<CtxValue> GetGlobalStrVar(
-      const unicode_string_view& name);
+      const unicode_string_view& name) override;
   virtual std::shared_ptr<CtxValue> GetGlobalObjVar(
-      const unicode_string_view& name);
+      const unicode_string_view& name) override;
   virtual std::shared_ptr<CtxValue> GetProperty(
       const std::shared_ptr<CtxValue>& object,
-      const unicode_string_view& name);
+      const unicode_string_view& name) override;
 
   virtual void RegisterGlobalModule(const std::shared_ptr<Scope>& scope,
-                                    const ModuleClassMap& modules);
+                                    const ModuleClassMap& modules) override;
   virtual void RegisterNativeBinding(const unicode_string_view& name,
                                      hippy::base::RegisterFunction fn,
-                                     void* data);
+                                     void* data) override;
 
-  virtual std::shared_ptr<CtxValue> CreateNumber(double number);
-  virtual std::shared_ptr<CtxValue> CreateBoolean(bool b);
+  virtual std::shared_ptr<CtxValue> CreateNumber(double number) override;
+  virtual std::shared_ptr<CtxValue> CreateBoolean(bool b) override;
   virtual std::shared_ptr<CtxValue> CreateString(
-      const unicode_string_view& string);
-  virtual std::shared_ptr<CtxValue> CreateUndefined();
-  virtual std::shared_ptr<CtxValue> CreateNull();
+      const unicode_string_view& string) override;
+  virtual std::shared_ptr<CtxValue> CreateUndefined() override;
+  virtual std::shared_ptr<CtxValue> CreateNull() override;
   virtual std::shared_ptr<CtxValue> CreateObject(
-      const unicode_string_view& json);
+      const unicode_string_view& json) override;
   virtual std::shared_ptr<CtxValue> CreateArray(
       size_t count,
-      std::shared_ptr<CtxValue> value[]);
+      std::shared_ptr<CtxValue> value[]) override;
   virtual std::shared_ptr<CtxValue> CreateMap(
       size_t count,
-      std::shared_ptr<CtxValue> value[]);
+      std::shared_ptr<CtxValue> value[]) override;
   virtual std::shared_ptr<CtxValue> CreateJsError(
-      const unicode_string_view& msg);
+      const unicode_string_view& msg) override;
 
   // Get From Value
   virtual std::shared_ptr<CtxValue> CallFunction(
       const std::shared_ptr<CtxValue>& function,
       size_t argument_count,
-      const std::shared_ptr<CtxValue> arguments[] = nullptr);
+      const std::shared_ptr<CtxValue> arguments[]) override;
 
-  virtual bool GetValueNumber(const std::shared_ptr<CtxValue>& value, double* result);
-  virtual bool GetValueNumber(const std::shared_ptr<CtxValue>& value, int32_t* result);
-  virtual bool GetValueBoolean(const std::shared_ptr<CtxValue>& value, bool* result);
+  virtual bool GetValueNumber(const std::shared_ptr<CtxValue>& value, double* result) override;
+  virtual bool GetValueNumber(const std::shared_ptr<CtxValue>& value, int32_t* result) override;
+  virtual bool GetValueBoolean(const std::shared_ptr<CtxValue>& value, bool* result) override;
   virtual bool GetValueString(const std::shared_ptr<CtxValue>& value,
-                              unicode_string_view* result);
+                              unicode_string_view* result) override;
   virtual bool GetValueJson(const std::shared_ptr<CtxValue>& value,
-                            unicode_string_view* result);
+                            unicode_string_view* result) override;
 
-  virtual bool IsMap(const std::shared_ptr<CtxValue>& value);
+  virtual bool IsMap(const std::shared_ptr<CtxValue>& value) override;
 
-  virtual bool IsNullOrUndefined(const std::shared_ptr<CtxValue>& value);
+  virtual bool IsNullOrUndefined(const std::shared_ptr<CtxValue>& value) override;
 
   // Array Helpers
 
-  virtual bool IsArray(const std::shared_ptr<CtxValue>& value);
-  virtual uint32_t GetArrayLength(const std::shared_ptr<CtxValue>& value);
+  virtual bool IsArray(const std::shared_ptr<CtxValue>& value) override;
+  virtual uint32_t GetArrayLength(const std::shared_ptr<CtxValue>& value) override;
   virtual std::shared_ptr<CtxValue> CopyArrayElement(const std::shared_ptr<CtxValue>& value,
-                                                     uint32_t index);
+                                                     uint32_t index) override;
 
   // Map Helpers
   virtual uint32_t GetMapLength(std::shared_ptr<CtxValue>& value);
@@ -200,29 +200,29 @@ class V8Ctx : public Ctx {
   // Object Helpers
 
   virtual bool HasNamedProperty(const std::shared_ptr<CtxValue>& value,
-                                const unicode_string_view& utf8name);
+                                const unicode_string_view& utf8name) override;
   virtual std::shared_ptr<CtxValue> CopyNamedProperty(
       const std::shared_ptr<CtxValue>& value,
-      const unicode_string_view& utf8name);
+      const unicode_string_view& utf8name) override;
   // Function Helpers
 
-  virtual bool IsFunction(const std::shared_ptr<CtxValue>& value);
-  virtual unicode_string_view CopyFunctionName(const std::shared_ptr<CtxValue>& value);
+  virtual bool IsFunction(const std::shared_ptr<CtxValue>& value) override;
+  virtual unicode_string_view CopyFunctionName(const std::shared_ptr<CtxValue>& value) override;
 
   virtual std::shared_ptr<CtxValue> RunScript(
       const unicode_string_view& data,
       const unicode_string_view& file_name,
       bool is_use_code_cache = false,
       unicode_string_view* cache = nullptr,
-      bool is_copy = true);
+      bool is_copy = true) override;
 
-  virtual std::shared_ptr<CtxValue> GetJsFn(const unicode_string_view& name);
-  virtual bool ThrowExceptionToJS(const std::shared_ptr<CtxValue>& exception);
+  virtual std::shared_ptr<CtxValue> GetJsFn(const unicode_string_view& name) override;
+  virtual bool ThrowExceptionToJS(const std::shared_ptr<CtxValue>& exception) override;
 
   virtual std::shared_ptr<JSValueWrapper> ToJsValueWrapper(
-      const std::shared_ptr<CtxValue>& value);
+      const std::shared_ptr<CtxValue>& value) override;
   virtual std::shared_ptr<CtxValue> CreateCtxValue(
-      const std::shared_ptr<JSValueWrapper>& wrapper);
+      const std::shared_ptr<JSValueWrapper>& wrapper) override;
 
   unicode_string_view ToStringView(v8::Local<v8::String> str) const;
   unicode_string_view GetMsgDesc(v8::Local<v8::Message> message);
