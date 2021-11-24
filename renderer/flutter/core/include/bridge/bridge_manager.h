343ae25cf01996959e21fd21b667e33b2880582f
lvfen <284437334@qq.com>
2021-11-24 11:45:04 +0800

    feat(voltron): voltron ffi code opt

---
+++ renderer/flutter/core/include/bridge/bridge_manager.h
@@ -0,0 +1,16 @@
+//
+// Created by longquan on 2021/11/23.
+//
+
+#ifndef VOLTRON_CORE_ANDROID_BRIDGE_MANAGER_H
+#define VOLTRON_CORE_ANDROID_BRIDGE_MANAGER_H
+
+
+
+class bridge_manager {
+
+};
+
+
+
+#endif //VOLTRON_CORE_ANDROID_BRIDGE_MANAGER_H
---
+++ renderer/flutter/core/src/bridge/bridge_manager.cc
@@ -0,0 +1,5 @@
+//
+// Created by longquan on 2021/11/23.
+//
+
+#include "bridge_manager.h"
