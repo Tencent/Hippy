639f4e87342164bbd83c66fe948c32da111719c0
zoomchan-cxj <zoom_chan@163.com>
2021-12-06 17:29:28 +0800

    build(core): build core js

--- core/src/napi/v8/native_source_code_android.cc
+++ core/src/napi/v8/native_source_code_android.cc
@@ -5,7 +5,7 @@
 * js2cpp is maintenance by Hippy Team <hippy@tencent.com>
 * Copyright © 2018-2021 Tencent. All rights reserved.
 *
-* Generated at Thu Nov 18 2021 15:30:08 GMT+0800 (China Standard Time).
+* Generated at Mon Dec 06 2021 17:15:09 GMT+0800 (China Standard Time).
 * DO NOT EDIT IT.
 */
 
