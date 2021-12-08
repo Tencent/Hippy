4ee0e62495ff5eb9c86eae64739655921d49a070
charleeshen <charleeshen@tencent.com>
2021-12-08 11:30:54 +0800

    feat(dom): add dom argument

--- dom/include/dom/dom_argument.h
+++ dom/include/dom/dom_argument.h
@@ -7,7 +7,6 @@ namespace hippy {
 inline namespace dom {
 
 enum class ArgumentType {
-  JSON,
   BSON,
   OBJECT,
 };
@@ -19,8 +18,6 @@ class DomArgument {
 
   DomArgument(const tdf::base::DomValue& dom_value)
       : data_(std::make_any<tdf::base::DomValue>(dom_value)), argument_type_(ArgumentType::OBJECT){};
-  DomArgument(const std::string& json_value)
-      : data_(std::make_any<std::string>(json_value)), argument_type_(ArgumentType::JSON){};
   DomArgument(const std::vector<uint8_t>& bson_value)
       : data_(std::make_any<std::vector<uint8_t>>(bson_value)), argument_type_(ArgumentType::BSON){};
 
@@ -28,18 +25,12 @@ class DomArgument {
 
   DomArgument& operator=(const DomArgument& rhs) noexcept;
 
-  bool ToJson(std::string& json);
   bool ToBson(std::vector<uint8_t>& bson);
   bool ToObject(tdf::base::DomValue& dom_value);
 
  private:
-  bool ConvertObjectToJson(const tdf::base::DomValue& dom_value, std::string& json);
-  bool ConvertBsonToJson(const std::vector<const uint8_t>& bson, std::string& json);
-
-  bool ConvertJsonToBson(const std::string& json, std::vector<uint8_t>& bson);
   bool ConvertObjectToBson(const tdf::base::DomValue& dom_value, std::vector<uint8_t>& bson);
 
-  bool ConvertJsonToObject(const std::string& json, tdf::base::DomValue& dom_value);
   bool ConvertBsonToObject(const std::vector<const uint8_t>& bson, tdf::base::DomValue& dom_value);
 
   std::any data_;
