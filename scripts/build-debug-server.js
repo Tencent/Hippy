eeb90977bc3c133876eced56e64ebbd2c062976f
chestershen <chestershen@tencent.com>
2021-12-27 15:35:02 +0800

    fix(build script): fix debug-server build script

--- scripts/build-debug-server.js
+++ scripts/build-debug-server.js
@@ -14,10 +14,8 @@ function buildDebugServer() {
   pushd(DEBUG_SERVER_PATH);
 
   const execOptions = { stdio: 'inherit' };
-  console.log('1/2 Start to install @hippy/debug-server dependencies');
-  exec('npm install --legacy-peer-deps', execOptions);
 
-  console.log('2/2 Start to build project @hippy/debug-server');
+  console.log('1/1 Start to build project @hippy/debug-server');
   exec('npm run build', execOptions);
 
   console.log('👌 All done, you can debug your hippy app now, enjoy.');
