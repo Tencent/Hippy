daf67d33c347b4c2071fe27d9d601edda91ed118
chestershen <chestershen@tencent.com>
2021-12-29 13:25:31 +0800

    feat(debug-server): add cli API

--- packages/hippy-debug-server/server/webpack.js
+++ packages/hippy-debug-server/server/webpack.js
@@ -7,12 +7,26 @@ const {
 } = require('../utils');
 const WebpackDevServer = require('../webpack-dev-server/lib/Server');
 
-const startWebpackDevServer = async (args) => {
-  const {
-    verbose,
-    config,
-  } = args;
-  const { webpackConfig, hmrPort } = getWebpackConfig(config);
+module.exports = {
+  webpack: (webpackConfig, cb) => {
+    const compiler = Webpack(webpackConfig, cb);
+    startWebpackDevServer(webpackConfig, compiler);
+    return compiler;
+  },
+  getWebpackConfig: (configPath) => {
+    let hmrPort;
+    let webpackConfig;
+    const webpackConfigPath = path.resolve(process.cwd(), configPath);
+    if (configPath && fs.existsSync(webpackConfigPath)) {
+      webpackConfig = require(webpackConfigPath);
+      hmrPort = (webpackConfig.devServer && webpackConfig.devServer.port) || 38988;
+    }
+    return { webpackConfig, hmrPort };
+  },
+};
+
+async function startWebpackDevServer(webpackConfig, compiler) {
+  const hmrPort = (webpackConfig.devServer && webpackConfig.devServer.port) || 38988;
   try  {
     if (hmrPort) {
       const reversePort = `tcp:${hmrPort}`;
@@ -21,27 +35,8 @@ const startWebpackDevServer = async (args) => {
   } catch (e) {
     logger.warn('Port reverse failed, For iOS app debug only just ignore the message.');
     logger.warn('Otherwise please check adb devices command working correctly');
-    if (verbose) {
-      logger.error(e);
-    }
   }
 
-  const compiler = Webpack(webpackConfig);
   const server = new WebpackDevServer(webpackConfig.devServer, compiler);
   await server.start();
-};
-
-
-function getWebpackConfig(configPath) {
-  let hmrPort;
-  let webpackConfig;
-  const webpackConfigPath = path.resolve(process.cwd(), configPath);
-  if (configPath && fs.existsSync(webpackConfigPath)) {
-    webpackConfig = require(webpackConfigPath);
-    hmrPort = (webpackConfig.devServer && webpackConfig.devServer.port) || 38988;
-  }
-  return { webpackConfig, hmrPort };
 }
-
-
-module.exports = { startWebpackDevServer };
