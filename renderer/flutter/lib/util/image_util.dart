33deee195432964c707f749b70b274a2a6d7f15a
lvfen <284437334@qq.com>
2021-12-30 16:49:48 +0800

    fix(voltron): support base64 image

--- renderer/flutter/lib/util/image_util.dart
+++ renderer/flutter/lib/util/image_util.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -8,8 +9,15 @@ ImageProvider getImage(String src) {
     image = AssetImage(src.replaceFirst('assets://', ''));
   } else if (src.startsWith('file://')) {
     image = FileImage(File(src.replaceFirst('file://', '')));
+  } else if (src.startsWith('data:')){ // base 64
+    var base64Str = src.split('base64,').last;
+    image = imageFromBase64String(base64Str);
   } else {
     image = NetworkImage(src);
   }
   return image;
+}
+
+MemoryImage imageFromBase64String(String base64String) {
+  return MemoryImage(base64Decode(base64String));
 }
