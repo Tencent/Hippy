e560eb4fb7a0d9e1293bc4fea61e610dbe5e23e7
lvfen <284437334@qq.com>
2021-12-30 19:02:27 +0800

    fix(dom): add startBatch

--- dom/include/dom/layer_optimized_render_manager.h
+++ dom/include/dom/layer_optimized_render_manager.h
@@ -15,6 +15,7 @@ class LayerOptimizedRenderManager : public RenderManager {
   void UpdateLayout(const std::vector<std::shared_ptr<DomNode>>& nodes) override;
   void MoveRenderNode(std::vector<int32_t>&& moved_ids, int32_t from_pid, int32_t to_pid) override;
   void Batch() override;
+  void StartBatch() override;
 
   void BeforeLayout() override;
   void AfterLayout() override;
