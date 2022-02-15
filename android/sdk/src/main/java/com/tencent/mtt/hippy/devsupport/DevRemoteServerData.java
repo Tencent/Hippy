457457ec8537b391c191dea7394d6add87aeafe8
ivanfanwu <ivanfanwu@tencent.com>
2022-02-15 10:56:52 +0800

    feat(android): support remote non usb debug

--- android/sdk/src/main/java/com/tencent/mtt/hippy/devsupport/DevRemoteServerData.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/devsupport/DevRemoteServerData.java
@@ -1,15 +1,21 @@
 package com.tencent.mtt.hippy.devsupport;
 
 import android.text.TextUtils;
+
 import com.tencent.mtt.hippy.utils.LogUtils;
-import java.net.MalformedURLException;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLDecoder;
 
 public class DevRemoteServerData {
 
-  private String host;
+  private static String KEY_DEBUG_URL = "debugURL";
+  private String scheme;  // protocol http or https
+  private String host;  // host:port address
   private String path;
   private String versionId;
+  private String wsUrl;  // ws debug url if remoteDebugUrl with
 
   public DevRemoteServerData(String remoteServerUrl) {
     parseUrl(remoteServerUrl);
@@ -27,8 +33,14 @@ public class DevRemoteServerData {
     }
     try {
       URL url = new URL(remoteServerUrl);
+      scheme = url.getProtocol();
       host = url.getHost();
       path = url.getPath();
+      int port = url.getPort();
+      if (port > 0) {
+        host = host + ":" + port;
+      }
+      wsUrl = parseQueryDebugUrl(url.getQuery());
       if (path.startsWith("/")) {
         path = path.substring(1); // remove first character /
       }
@@ -41,6 +53,30 @@ public class DevRemoteServerData {
     } catch (Exception e) {
       LogUtils.e("Hippy DevRemoteServerData", "parseUrl error", e);
     }
+  }
+
+  /**
+   * parse the ws debug url when remoteDebugUrl with
+   *
+   * @param query query params
+   * @return debugUrl value
+   * @throws UnsupportedEncodingException not support encoding
+   */
+  private String parseQueryDebugUrl(String query) throws UnsupportedEncodingException {
+    if (TextUtils.isEmpty(query)) {
+      return null;
+    }
+    String[] queryList = query.split("&");
+    for (String queryItem : queryList) {
+      int idPosition = queryItem.indexOf("=");
+      if (idPosition >= 0) {
+        String findKey = queryItem.substring(0, idPosition);
+        if (KEY_DEBUG_URL.equals(findKey)) {
+          return URLDecoder.decode(queryItem.substring(idPosition + 1), "UTF-8");
+        }
+      }
+    }
+    return null;
   }
 
   public boolean isValid() {
@@ -57,5 +93,13 @@ public class DevRemoteServerData {
 
   public String getPath() {
     return path;
+  }
+
+  public String getScheme() {
+    return scheme;
+  }
+
+  public String getWsUrl() {
+    return wsUrl;
   }
 }
