142529be2f36f430ade4925ed3df28bc0706123e
thomasyqguo <thomasyqguo@tencent.com>
2022-02-22 15:23:10 +0800

    feat:截图请求接口增加最大宽高
    融合框架截图功能
    
    --issue=#127

--- framework/js/core/include/devtools/adapter/hippy_screen_adapter.h
+++ framework/js/core/include/devtools/adapter/hippy_screen_adapter.h
@@ -12,21 +12,30 @@
 
 namespace hippy {
 namespace devtools {
-static const char *const kScreenShot = "screenShot";
-static const char *const kScreenWidth = "width";
-static const char *const kScreenHeight = "height";
+constexpr const char* kScreenShot = "screenShot";
+constexpr const char* kScreenWidth = "width";
+constexpr const char* kScreenHeight = "height";
+constexpr const char* kMaxWidth = "maxWidth";
+constexpr const char* kMaxHeight = "maxHeight";
+constexpr const char* kQuality = "quality";
+
 class HippyScreenAdapter : public tdf::devtools::ScreenAdapter {
  public:
   explicit HippyScreenAdapter(int32_t dom_id) : dom_id_(dom_id) {}
   void GetScreenShot(const tdf::devtools::ScreenRequest& request, CoreScreenshotCallback callback) override {
     if (callback) {
-      std::function func = [this, callback] {
+      std::function func = [this, callback, request] {
         std::shared_ptr<DomManager> dom_manager = DomManager::Find(static_cast<int32_t>(dom_id_));
         if (dom_manager) {
           auto root_node = dom_manager->GetNode(dom_manager->GetRootId());
           auto children = root_node->GetChildren();
           if (!children.empty()) {
-            hippy::dom::DomArgument argument;
+            tdf::base::DomValue::DomValueObjectType domValueObject;
+            domValueObject[kMaxWidth] = tdf::base::DomValue(request.req_width);
+            domValueObject[kMaxHeight] = tdf::base::DomValue(request.req_height);
+            domValueObject[kQuality] = tdf::base::DomValue(request.quality);
+            tdf::base::DomValue domValue(domValueObject);
+            hippy::dom::DomArgument argument(domValue);
             std::function screen_shot_callback = [this, callback](std::shared_ptr<DomArgument> arg) {
               tdf::base::DomValue result_dom_value;
               arg->ToObject(result_dom_value);
