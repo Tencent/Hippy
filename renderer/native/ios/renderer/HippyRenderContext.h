3575d75cd2da2be01de7035b5c5eef3c1e206fbd
mengyanluo <mengyanluo@tencent.com>
2022-03-05 20:22:50 +0800

    feat(ios): add render context protocol file

---
+++ renderer/native/ios/renderer/HippyRenderContext.h
@@ -0,0 +1,17 @@
+//
+//  HippyRenderContext.h
+//  HippyDemo
+//
+//  Created by mengyanluo on 2022/3/5.
+//  Copyright © 2022 tencent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol HippyRenderContext <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
