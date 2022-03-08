3c57f37a229fb3b4677c390a491e65c4ce85b46d
ozonelmy <ozonelmy@163.com>
2022-03-08 10:30:04 +0800

    feat(ios): add checks for file url

--- ios/sdk/utils/HippyUtils.m
+++ ios/sdk/utils/HippyUtils.m
@@ -769,7 +769,15 @@ NSURL *__nullable HippyURLWithString(NSString *URLString, NSString *baseURLStrin
         if (nil == uriData) {
             return nil;
         }
-        CFURLRef urlRef = CFURLCreateWithBytes(NULL, [uriData bytes], [uriData length], kCFStringEncodingUTF8, (__bridge CFURLRef)baseURL);
+        CFURLRef urlRef = NULL;
+        if ([URLString hasPrefix:@"http"] ||
+            [URLString hasPrefix:@"data:"] ||
+            [URLString hasPrefix:@"file:"]) {
+            urlRef = CFURLCreateWithBytes(NULL, [uriData bytes], [uriData length], kCFStringEncodingUTF8, (__bridge CFURLRef)baseURL);
+        }
+        else {
+            urlRef = CFURLCreateWithFileSystemPath(NULL, (__bridge CFStringRef)URLString, kCFURLPOSIXPathStyle, NO);
+        }
         NSURL *source_url = CFBridgingRelease(urlRef);
         return source_url;
     }
