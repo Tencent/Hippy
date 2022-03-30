4f604642f4958d3fa94a35c43847e4282c17253e
ilikethese <shenli0124@qq.com>
2022-03-30 11:22:10 +0800

    Revert "feat(dom): add event listener and remove listener"
    This reverts commit 97210ba2b604fd833a086c71814d783d5a29a69f.

--- framework/js/core/include/core/modules/ui_manager_module.h
+++ framework/js/core/include/core/modules/ui_manager_module.h
@@ -50,10 +50,6 @@ class UIManagerModule : public ModuleBase {
   void StartBatch(const CallbackInfo& info);
   void EndBatch(const CallbackInfo& info);
   void CallUIFunction(const CallbackInfo& info);
-  void AddEventListener(const CallbackInfo& info);
-  void AddEventListeners(const CallbackInfo &info);
-  void RemoveEventListener(const CallbackInfo &info);
-  void RemoveEventListeners(const CallbackInfo &info);
 
  private:
 };
