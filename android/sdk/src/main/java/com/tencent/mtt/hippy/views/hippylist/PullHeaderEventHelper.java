2f35daf765c67ced47ab783f46d0eead3d09004e
maxli <maxli@tencent.com>
2022-04-12 17:46:01 +0800

    fix(android): reset pull header position for recycler view

--- android/sdk/src/main/java/com/tencent/mtt/hippy/views/hippylist/PullHeaderEventHelper.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/views/hippylist/PullHeaderEventHelper.java
@@ -128,4 +128,9 @@ public class PullHeaderEventHelper implements IHeaderRefreshListener, IHeaderRef
     public void requestLayout() {
         recyclerView.dispatchLayout();
     }
+
+    @Override
+    public void resetPullHeaderPositionIfNeeded() {
+        headerRefreshHelper.resetHeaderViewPosition();
+    }
 }
