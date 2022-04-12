facb3bc0fd23eb17a19a087d874dc04c0cdfa0ea
corinli <corinli@tencent.com>
2022-04-12 20:25:43 +0800

    chore(hippy-react-demo): support web dev hot reload

--- examples/hippy-react-demo/scripts/hippy-webpack.web.dev.js
+++ examples/hippy-react-demo/scripts/hippy-webpack.web.dev.js
@@ -41,6 +41,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.(jsx?)$/,
         use: [
           {
@@ -86,7 +91,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
     modules: [path.resolve(__dirname, '../node_modules')],
     alias: (() => {
       const aliases = {
@@ -94,8 +99,8 @@ module.exports = {
       };
       // If hippy-react-web was built exist then make a alias to @hippy/react
       // Remove the section if you don't use it
-      const hippyReactPath = path.resolve(__dirname, '../../../packages/hippy-react-web');
-      if (fs.existsSync(path.resolve(hippyReactPath, 'dist/index.js'))) {
+      const hippyReactPath = path.resolve(__dirname, '../../../packages/hippy-react-web/src/index.ts');
+      if (fs.existsSync(path.resolve(hippyReactPath))) {
         console.warn(`* Using the @hippy/react in ${hippyReactPath}`);
         aliases['@hippy/react'] = hippyReactPath;
       } else {
