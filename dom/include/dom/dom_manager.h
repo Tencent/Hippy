d1da973e587c991efc854b356a9408a7abe028c8
jinhaihuang <jinhaihuang@tencent.com>
2022-04-15 14:57:04 +0800

    feat(dom): implement RootNode

--- dom/include/dom/dom_manager.h
+++ dom/include/dom/dom_manager.h
@@ -21,6 +21,7 @@ inline namespace dom {
 
 class DomNode;
 class RenderManager;
+class RootNode;
 class LayerOptimizedRenderManager;
 
 class DomManager : public std::enable_shared_from_this<DomManager> {
@@ -36,10 +37,8 @@ class DomManager : public std::enable_shared_from_this<DomManager> {
   inline std::shared_ptr<RenderManager> GetRenderManager() { return render_manager_.lock(); }
   void SetRenderManager(std::shared_ptr<RenderManager> render_manager);
   inline void SetDelegateTaskRunner(std::shared_ptr<TaskRunner> runner) { delegate_task_runner_ = runner; }
-  inline uint32_t GetRootId() { return root_id_; }
-  inline std::shared_ptr<DomNode> GetNode(uint32_t id) {
-    return dom_node_registry_.GetNode(hippy::base::checked_numeric_cast<uint32_t , int32_t>(id));
-  }
+  uint32_t GetRootId() const;
+  std::shared_ptr<DomNode> GetNode(uint32_t id) const;
 
   void CreateDomNodes(std::vector<std::shared_ptr<DomNode>>&& nodes);
   void UpdateDomNodes(std::vector<std::shared_ptr<DomNode>>&& nodes);
@@ -52,10 +51,9 @@ class DomManager : public std::enable_shared_from_this<DomManager> {
   void CallFunction(uint32_t id, const std::string& name, const DomArgument& param, const CallFunctionCallback& cb);
   std::tuple<float, float> GetRootSize();
   void SetRootSize(float width, float height);
-  void SetRootNode(const std::shared_ptr<DomNode>& root_node);
+  void SetRootNode(const std::shared_ptr<RootNode>& root_node);
   void DoLayout();
   void DoLayout(std::function<void()> func);
-  void DoNotifyRender();
   void PostTask(std::function<void()> func);
   void StartTaskRunner() { dom_task_runner_->Start(); }
   void TerminateTaskRunner() { dom_task_runner_->Terminate(); }
@@ -66,42 +64,18 @@ class DomManager : public std::enable_shared_from_this<DomManager> {
 
  private:
   int32_t id_;
-  uint32_t root_id_;
-  std::shared_ptr<DomNode> root_node_;
+  std::shared_ptr<RootNode> root_node_;
   std::shared_ptr<LayerOptimizedRenderManager> optimized_render_manager_;
   std::weak_ptr<RenderManager> render_manager_;
   std::weak_ptr<TaskRunner> delegate_task_runner_;
   std::shared_ptr<TaskRunner> dom_task_runner_;
 
-  class DomNodeRegistry {
-   public:
-    void AddNode(const std::shared_ptr<DomNode>& node);
-    std::shared_ptr<DomNode> GetNode(int32_t id);
-    void RemoveNode(int32_t id);
-
-   private:
-    std::map<int32_t, std::shared_ptr<DomNode>> nodes_;
-  };
-
-  DomNodeRegistry dom_node_registry_;
-
-  using DomOperation = std::function<void(void)>;
-  std::vector<DomOperation> batched_operations_;
-  std::vector<DomOperation> listener_operations_;
-  std::vector<DomOperation> layout_operations_;
-  std::vector<std::shared_ptr<DomNode>> layout_changed_nodes_;
   void HandleEvent(const std::shared_ptr<DomEvent>& event);
-  void AddLayoutChangedNode(const std::shared_ptr<DomNode>& node);
   void AddEventListenerOperation(const std::shared_ptr<DomNode>& node, const std::string& name);
   void RemoveEventListenerOperation(const std::shared_ptr<DomNode>& node, const std::string& name);
-  void DeleteDomNode(const std::shared_ptr<DomNode>& node);
   void UpdateRenderNode(const std::shared_ptr<DomNode>& node);
 
-  void NotifyBuildRenderTree();
-  void BindEvent();
   void Layout();
-  void NotifyRender();
-
 
   friend DomNode;
 };
---
+++ dom/include/dom/root_node.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "dom/dom_node.h"
+
+namespace hippy {
+inline namespace dom {
+
+class RootNode : public DomNode {
+ public:
+  RootNode(uint32_t id);
+
+  void CreateDomNodes(std::vector<std::shared_ptr<DomNode>>&& nodes);
+  void UpdateDomNodes(std::vector<std::shared_ptr<DomNode>>&& nodes);
+  void DeleteDomNodes(std::vector<std::shared_ptr<DomNode>>&& nodes);
+  void SyncWithRenderManager(std::shared_ptr<RenderManager> render_manager);
+  void DoAndFlushLayout(std::shared_ptr<RenderManager> render_manager);
+
+  void AddEvent(uint32_t id, const std::string& event_name);
+  void RemoveEvent(uint32_t id, const std::string& event_name);
+
+  std::shared_ptr<DomNode> GetNode(uint32_t id);
+
+ private:
+  struct DomOperation {
+    static constexpr int kOpCreate = 1;
+    static constexpr int kOpUpdate = 2;
+    static constexpr int kOpDelete = 3;
+
+    int32_t op;
+    std::vector<std::shared_ptr<DomNode>> nodes;
+  };
+
+  struct EventOperation {
+    static constexpr int kOpAdd = 1;
+    static constexpr int kOpRemove = 2;
+
+    int32_t op;
+    uint32_t id;
+    std::string name;
+  };
+
+  std::vector<DomOperation> dom_operations_;
+  std::vector<EventOperation> event_operations_;
+
+  void FlushDomOperations(std::shared_ptr<RenderManager> render_manager);
+  void FlushEventOperations(std::shared_ptr<RenderManager> render_manager);
+
+  void OnDomNodeCreated(const std::shared_ptr<DomNode>& node);
+  void OnDomNodeDeleted(const std::shared_ptr<DomNode>& node);
+
+  std::unordered_map<uint32_t, std::weak_ptr<DomNode>> nodes_;
+};
+
+}  // namespace dom
+}  // namespace hippy
