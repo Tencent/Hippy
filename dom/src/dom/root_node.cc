6294541efdfb7946f26dbc0db071e5bb8ed39014
jinhaihuang <jinhaihuang@tencent.com>
2022-04-18 16:11:14 +0800

    feat(dom): pass DomManager to RootNode when initializing it

--- dom/src/dom/root_node.cc
+++ dom/src/dom/root_node.cc
@@ -10,10 +10,11 @@ constexpr char kOnDomCreated[] = "onDomCreated";
 constexpr char kOnDomUpdated[] = "onDomUpdated";
 constexpr char kOnDomDeleted[] = "onDomDeleted";
 
-RootNode::RootNode(uint32_t id)
-        : DomNode(id, 0, 0) {
-  SetExtStyleMap(std::make_shared<std::unordered_map<std::string, std::shared_ptr<DomValue>>>());
-}
+RootNode::RootNode(uint32_t id, const std::weak_ptr<DomManager> &dom_manager)
+        : DomNode(id, 0, 0, "", "",
+                  std::unordered_map<std::string, std::shared_ptr<DomValue>>(),
+                  std::unordered_map<std::string, std::shared_ptr<DomValue>>(),
+                  dom_manager.lock()) {}
 
 void RootNode::CreateDomNodes(std::vector<std::shared_ptr<DomNode>>&& nodes) {
   std::vector<std::shared_ptr<DomNode>> nodes_to_create;
