7fdf35a0b6819e8f01688fa6a91c46e981d7cff1
nolantang <nolantang@tencent.com>
2022-04-22 17:00:07 +0800

    feat(devtools): modify src file copyright information

--- devtools/devtools-backend/src/module/model/tdf_inspector_model.cc
+++ devtools/devtools-backend/src/module/model/tdf_inspector_model.cc
@@ -1,7 +1,22 @@
-//
-// Copyright (c) 2021 Tencent Corporation. All rights reserved.
-// Created by ivanfanwu on 2021/9/16.
-//
+/*
+ * Tencent is pleased to support the open source community by making
+ * Hippy available.
+ *
+ * Copyright (C) 2017-2019 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "module/model/tdf_inspector_model.h"
 #include <sstream>
