d5ace9e8790407cbcee65ef512b6a4b255b2aef3
zoomchan-cxj <zoom_chan@163.com>
2022-05-04 20:52:10 +0800

    feat(react): change updateNode to batch operation & perf event binding

--- packages/hippy-react/src/renderer/render.ts
+++ packages/hippy-react/src/renderer/render.ts
@@ -153,12 +153,6 @@ function getTargetNodeAttributes(targetNode: Element) {
       id: targetNode.id,
       ...targetNodeAttributes,
     };
-    // delete special __bind__event attribute, which is used in C DOM
-    Object.keys(attributes).forEach((key) => {
-      if (key.indexOf('__bind__') === 0 && typeof attributes[key] === 'boolean') {
-        delete attributes[key];
-      }
-    });
     delete attributes.text;
     delete attributes.value;
     return attributes;
@@ -285,7 +279,6 @@ function insertChild(parentNode: ViewNode, childNode: ViewNode, atIndex = -1) {
       type: NODE_OPERATION_TYPES.createNode,
       nodes: translated,
     });
-    // endBatch();
   }
 }
 
@@ -305,7 +298,6 @@ function removeChild(parentNode: ViewNode, childNode: ViewNode | null, index: nu
     type: NODE_OPERATION_TYPES.deleteNode,
     nodes: deleteNodeIds,
   });
-  // endBatch();
 }
 
 function updateChild(parentNode: Element) {
@@ -320,7 +312,6 @@ function updateChild(parentNode: Element) {
       nodes: [translated],
     });
   }
-  // endBatch();
 }
 
 function updateWithChildren(parentNode: ViewNode) {
@@ -333,7 +324,6 @@ function updateWithChildren(parentNode: ViewNode) {
     type: NODE_OPERATION_TYPES.updateNode,
     nodes: translated,
   });
-  // endBatch();
 }
 
 export {
