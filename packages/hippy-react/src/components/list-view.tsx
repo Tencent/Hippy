a0eda1b86059dcc30c4301b4dbfb43e9d6e98c34
zoomchan-cxj <zoom_chan@163.com>
2022-05-04 20:52:10 +0800

    feat(react): change updateNode to batch operation & perf event binding

--- packages/hippy-react/src/components/list-view.tsx
+++ packages/hippy-react/src/components/list-view.tsx
@@ -425,7 +425,9 @@ class ListView extends React.Component<ListViewProps, ListViewState> {
       }
       const appearEventList = [onAppear, onDisappear, onWillAppear, onWillDisappear];
       nativeProps.exposureEventEnabled = appearEventList.some(func => typeof func === 'function');
-      nativeProps.numberOfRows = itemList.length;
+      if (Device.platform.OS === 'ios') {
+        nativeProps.numberOfRows = itemList.length;
+      }
       (nativeProps as ListViewProps).initialListSize = initialListSize;
       (nativeProps as ListViewProps).style = {
         overflow: 'scroll',
