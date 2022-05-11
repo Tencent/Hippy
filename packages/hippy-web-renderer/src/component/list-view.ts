676118dd5e7715be7b3cc317a62e8b28c57faee7
pba-cra <syb233@icloud.com>
2022-05-11 15:52:53 +0800

    fix(listview): change list-view sticky row zIndex
    (cherry picked from commit 79e0206c314837a3fe4af60e29cc9d6b8318073b)

--- packages/hippy-web-renderer/src/component/list-view.ts
+++ packages/hippy-web-renderer/src/component/list-view.ts
@@ -340,7 +340,7 @@ export class ListView extends HippyView<HTMLDivElement> {
     if (this.stickyContainer === null) {
       this.stickyContainer = document.createElement('div');
       const rect = this.dom?.getBoundingClientRect() ?? { y: 0, x: 0, width: 0 };
-      setElementStyle(this.stickyContainer, { position: 'fixed', top: rect.y, left: rect.x, width: rect.width, zIndex: 99999 });
+      setElementStyle(this.stickyContainer, { position: 'fixed', top: rect.y, left: rect.x, width: rect.width, zIndex: 0 });
       this.dom!.append(this.stickyContainer);
     }
     const children = this.stickyListViewItem!.dom!.childNodes;
