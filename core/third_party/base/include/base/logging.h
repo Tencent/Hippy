6be17f15a2a608ee81ea9339165b51cbf187f6c5
pollyzhang <pollyzhang@tencent.com>
2022-05-20 19:18:23 +0800

    fix(core): fix iOS warning

--- core/third_party/base/include/base/logging.h
+++ core/third_party/base/include/base/logging.h
@@ -22,7 +22,7 @@ inline std::ostream& operator<<(std::ostream& stream, const unicode_string_view&
     case unicode_string_view::Encoding::Latin1: {
       std::string u8;
       for (const auto& ch : str_view.latin1_value()){
-        if (ch < 0x80) {
+        if (static_cast<uint8_t>(ch) < 0x80) {
           u8 += ch;
         } else {
           u8 += (0xc0 | ch >> 6);
