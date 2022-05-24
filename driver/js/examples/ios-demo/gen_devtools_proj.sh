fc227d7a6188d48e4cd986c020d4b73f8d4565bd
nolantang <nolantang@tencent.com>
2022-05-24 16:18:16 +0800

    feat(devtools): fix ios compile issue

--- driver/js/examples/ios-demo/gen_devtools_proj.sh
+++
@@ -1,2 +0,0 @@
-echo "generate core project"
-cmake ../../../../devtools/devtools-backend/CMakeLists.txt -B ./devtools_backend -G Xcode -DMODULE_TOOLS=YES -DCMAKE_TOOLCHAIN_FILE=./ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=YES -DENABLE_ARC=YES -DTDF_SERVICE_ENABLED=YES
