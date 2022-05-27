2a3e32b00ed2f731b2d3ef72e2f302559166374b
zoomchan-cxj <zoom_chan@163.com>
2022-05-27 16:32:16 +0800

    refactor(core): fix ios invalid callback name

--- core/js/global/ios/Turbo.js
+++ core/js/global/ios/Turbo.js
@@ -4,17 +4,17 @@
 function turboPromise(func) {
   return function (...args) {
     return new Promise((resolve, reject) => {
-      const failCallbackId = __GLOBAL__._callbackID;
-      __GLOBAL__._callbacks[failCallbackId] = (data) => {
+      const successCallbackId = __GLOBAL__._callbackID;
+      __GLOBAL__._callbacks[successCallbackId] = (data) => {
         resolve(data);
       };
       __GLOBAL__._callbackID += 1;
-      const successCallbackId = __GLOBAL__._callbackID;
-      __GLOBAL__._callbacks[successCallbackId] = (errorData) => {
+      const failCallbackId = __GLOBAL__._callbackID;
+      __GLOBAL__._callbacks[failCallbackId] = (errorData) => {
         reject(errorData);
       };
       __GLOBAL__._callbackID += 1;
-      func.apply(this, [...args, failCallbackId, successCallbackId]);
+      func.apply(this, [...args, successCallbackId, failCallbackId]);
     });
   };
 }
