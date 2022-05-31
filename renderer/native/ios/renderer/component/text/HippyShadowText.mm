31398bacfab8fa07542eaf5b83a00a50667a452b
mengyanluo <mengyanluo@tencent.com>
2022-05-31 14:37:52 +0800

    fix(ios): adjust text layout

--- renderer/native/ios/renderer/component/text/HippyShadowText.mm
+++ renderer/native/ios/renderer/component/text/HippyShadowText.mm
@@ -507,8 +507,9 @@ static void resetFontAttribute(NSTextStorage *textStorage) {
             maxHeight = ceilf(heightOfTallestSubview);
         }
         paragraphStyle.lineHeightMultiple = _lineHeightMultiple;
+        maxHeight = MAX(maxHeight, maximumFontLineHeight);
         paragraphStyle.minimumLineHeight = lineHeight;
-//        paragraphStyle.maximumLineHeight = maxHeight;
+        paragraphStyle.maximumLineHeight = maxHeight;
         [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:(NSRange) { 0, attributedString.length }];
 
         /**
@@ -754,8 +755,6 @@ HIPPY_TEXT_PROPERTY(TextShadowColor, _textShadowColor, UIColor *);
     if (version >= 10.0 && version < 12.0) {
         text = [text stringByReplacingOccurrencesOfString:@"జ్ఞ‌ా" withString:@" "];
     }
-
-    text = [text stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
     if (_text != text && ![_text isEqualToString:text]) {
         _text = [text copy];
         [self dirtyText];
