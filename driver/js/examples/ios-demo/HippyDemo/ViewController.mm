f5e7cdd94158700f7b95ca273ff9196d5dc7ade8
charleeshen <charleeshen@tencent.com>
2022-06-02 11:24:07 +0800

    feat(dom): remove demo for vl

--- driver/js/examples/ios-demo/HippyDemo/ViewController.mm
+++ driver/js/examples/ios-demo/HippyDemo/ViewController.mm
@@ -57,7 +57,6 @@
         NSLog(@"hippy says:%@ in file %@ at line %@", message, fileName, lineNumber);
     });
     [self runCommonDemo];
-//    [self runDemoWithoutRuntime];
 }
 
 - (void)runCommonDemo {
@@ -108,101 +107,6 @@
 
     rootView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     [self.view addSubview:rootView];
-}
-
-- (void)runDemoWithoutRuntime {
-    NSString *businessBundlePath = [[NSBundle mainBundle] pathForResource:@"index.ios" ofType:@"js" inDirectory:@"res"];
-    businessBundlePath = [businessBundlePath stringByDeletingLastPathComponent];
-
-    //1.set up root view
-    UIView *view = [[UIView alloc] initWithFrame:self.view.bounds];
-    view.backgroundColor = [UIColor whiteColor];
-    view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    
-    //2.set root tag for root view. root tag is of the essence
-    int32_t rootTag = 10;
-    view.hippyTag = @(rootTag);
-    [self.view addSubview:view];
-
-    //3.set native render manager's frameworkProxy to bridge
-    [self initRenderContextWithRootView:view];
-
-    //4.mock data
-    auto nodesData = [self mockNodesData];
-    
-    //5.create dom nodes with datas
-    //TODO 这个demo流程需要删除
-//    _domManager->CreateDomNodes(std::move(nodesData));
-    
-    //6.end batch
-    _domManager->EndBatch();
-}
-
-- (void)initRenderContextWithRootView:(UIView *)rootView {
-    int hippyTag = [[rootView hippyTag] intValue];
-    HippyAssert(0 != hippyTag && 0 == hippyTag % 10, @"Root view's tag must not be 0 and must be a multiple of 10");
-    if (rootView && hippyTag) {
-        _domManager = std::make_shared<hippy::DomManager>(hippyTag);
-        _domManager->StartTaskRunner();
-        _domManager->SetRootSize(CGRectGetWidth(rootView.bounds), CGRectGetHeight(rootView.bounds));
-
-        _nativeRenderManager = std::make_shared<NativeRenderManager>();
-        _nativeRenderManager->SetFrameworkProxy(self);
-        _nativeRenderManager->RegisterRootView(rootView);
-        _nativeRenderManager->SetDomManager(_domManager);
-        _nativeRenderManager->RegisterExtraComponent(@{@"MyView": [MyViewManager class]});
-        _domManager->SetRenderManager(_nativeRenderManager);
-    }
-}
-
-- (std::vector<std::shared_ptr<hippy::DomNode>>) mockNodesData {
-    std::vector<std::shared_ptr<hippy::DomNode>> dom_node_vector;
-    using StyleMap = std::unordered_map<std::string, std::shared_ptr<tdf::base::DomValue>>;
-    NSString *mockDataPath = [[NSBundle mainBundle] pathForResource:@"create_node" ofType:@"json"];
-    NSData *mockData = [NSData dataWithContentsOfFile:mockDataPath];
-    NSArray<NSDictionary<NSString *, id> *> *mockJson = [NSJSONSerialization JSONObjectWithData:mockData options:(NSJSONReadingOptions)0 error:nil];
-    for (NSDictionary<NSString *, id> *mockNode in mockJson) {
-        uint32_t tag = 0;
-        uint32_t pid = 0;
-        uint32_t index = 0;
-        StyleMap style_map;
-        StyleMap ext_map;
-        std::string name;
-        for (NSString *key in mockNode) {
-            if ([key isEqualToString:@"id"]) {
-                tag = [mockNode[@"id"] unsignedIntValue];
-            }
-            else if ([key isEqualToString:@"pId"]) {
-                pid = [mockNode[@"pId"] unsignedIntValue];
-            }
-            else if ([key isEqualToString:@"index"]) {
-                index = [mockNode[@"index"] unsignedIntValue];
-            }
-            else if ([key isEqualToString:@"props"]) {
-                id props = mockNode[key];
-                auto all_props = dictionaryToUnorderedMapDomValue(props);
-                auto style_props = all_props["style"];
-                if (style_props) {
-                    if (tdf::base::DomValue::Type::kObject == style_props->GetType()) {
-                        auto style_object = style_props->ToObjectChecked();
-                        for (auto iter = style_object.begin(); iter != style_object.end(); iter++) {
-                            const std::string &iter_key = iter->first;
-                            auto &iter_value = iter->second;
-                            style_map[iter_key] = std::make_shared<tdf::base::DomValue>(std::move(iter_value));
-                        }
-                    }
-                    all_props.erase("style");
-                }
-                ext_map.swap(all_props);
-            }
-            else if ([key isEqualToString:@"name"]) {
-                name.assign([mockNode[key] UTF8String]);
-            }
-        }
-        std::shared_ptr<hippy::DomNode> dom_node = std::make_shared<hippy::DomNode>(tag, pid, name, name, std::move(style_map), std::move(ext_map), _domManager);
-        dom_node_vector.push_back(dom_node);
-    }
-    return dom_node_vector;
 }
 
 - (NSDictionary *)objectsBeforeExecuteCode {
