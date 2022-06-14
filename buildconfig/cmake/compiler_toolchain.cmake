350e0a0a89d499562cfa16860b3c2994e94e1cd7
superzheng <superzheng@tencent.com>
2022-06-14 18:21:43 +0800

    chore(cmake): introduce `GlobalPackages` to improve project cohesion

--- buildconfig/cmake/compiler_toolchain.cmake
+++ buildconfig/cmake/compiler_toolchain.cmake
@@ -19,59 +19,59 @@
 #
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    set(COMPILE_OPTIONS
-            -fomit-frame-pointer
-            -fno-threadsafe-statics
-            -fno-strict-aliasing
-            -fno-short-enums
-            -fno-unique-section-names
-            -fno-trigraphs
-            -Werror
-            -Wall
-            -Wextra
-            -Wextra-semi
-            -Wconversion
-            -Wimplicit-fallthrough
-            -Wloop-analysis
-            -Wmissing-field-initializers
-            -Wunused-local-typedefs
-            -Wstring-conversion
-            -Wthread-safety
-            -Wtautological-overlap-compare
-            -Wunreachable-code
-            -Wenum-compare-conditional
-            -Wheader-hygiene
-            -Wshadow
-            -Wno-unused-parameter
-            -Wno-trigraphs
-            --param=ssp-buffer-size=4
-            -pipe
-            -Os)
+  set(COMPILE_OPTIONS
+      -fomit-frame-pointer
+      -fno-threadsafe-statics
+      -fno-strict-aliasing
+      -fno-short-enums
+      -fno-unique-section-names
+      -fno-trigraphs
+      -Werror
+      -Wall
+      -Wextra
+      -Wextra-semi
+      -Wconversion
+      -Wimplicit-fallthrough
+      -Wloop-analysis
+      -Wmissing-field-initializers
+      -Wunused-local-typedefs
+      -Wstring-conversion
+      -Wthread-safety
+      -Wtautological-overlap-compare
+      -Wunreachable-code
+      -Wenum-compare-conditional
+      -Wheader-hygiene
+      -Wshadow
+      -Wno-unused-parameter
+      -Wno-trigraphs
+      --param=ssp-buffer-size=4
+      -pipe
+      -Os)
 
-    if (ANDROID_NDK)
-        if (${ANDROID_ABI} STREQUAL "armeabi-v7a")
-            list(APPEND COMPILE_OPTIONS -mfloat-abi=softfp)
-        elseif(${ANDROID_ABI} STREQUAL "arm64-v8a")
-            # Before LLVM 14 [1],
-            # Outline atomics will crash on Samsung Exynos 9810 CPU [2]
-            # with big cores are ARMv8.2 and LITTLE are ARMv8.0.
-            # Since LSE is for ARMv8.1 and later, so it should be disabled.
-            #
-            # [1] https://reviews.llvm.org/rGcce4a7258b81159e57a411896011ee2742f17def
-            # [2] https://bugs.chromium.org/p/chromium/issues/detail?id=1272795
-            if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 14)
-                list(APPEND COMPILE_OPTIONS -mno-outline-atomics)
-            endif()
-        elseif (${ANDROID_ABI} STREQUAL "x86")
-            list(APPEND COMPILE_OPTIONS
-                    -m32
-                    -mssse3
-                    -mfpmath=sse)
-        elseif (${ANDROID_ABI} STREQUAL "x86_64")
-            list(APPEND COMPILE_OPTIONS
-                    -m64
-                    -mpopcnt
-                    -msse4.2)
-        endif()
-    endif()
-endif()
+  if (ANDROID_NDK)
+    if (${ANDROID_ABI} STREQUAL "armeabi-v7a")
+      list(APPEND COMPILE_OPTIONS -mfloat-abi=softfp)
+    elseif (${ANDROID_ABI} STREQUAL "arm64-v8a")
+      # Before LLVM 14 [1],
+      # Outline atomics will crash on Samsung Exynos 9810 CPU [2]
+      # with big cores are ARMv8.2 and LITTLE are ARMv8.0.
+      # Since LSE is for ARMv8.1 and later, so it should be disabled.
+      #
+      # [1] https://reviews.llvm.org/rGcce4a7258b81159e57a411896011ee2742f17def
+      # [2] https://bugs.chromium.org/p/chromium/issues/detail?id=1272795
+      if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 14)
+        list(APPEND COMPILE_OPTIONS -mno-outline-atomics)
+      endif ()
+    elseif (${ANDROID_ABI} STREQUAL "x86")
+      list(APPEND COMPILE_OPTIONS
+          -m32
+          -mssse3
+          -mfpmath=sse)
+    elseif (${ANDROID_ABI} STREQUAL "x86_64")
+      list(APPEND COMPILE_OPTIONS
+          -m64
+          -mpopcnt
+          -msse4.2)
+    endif ()
+  endif ()
+endif ()
