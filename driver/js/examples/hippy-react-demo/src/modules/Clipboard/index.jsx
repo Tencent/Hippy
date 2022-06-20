7907413e4e34ee286dca47b68ff564683e9bc9b3
zoomchan-cxj <zoom_chan@163.com>
2022-06-20 18:20:23 +0800

    refactor(examples): add borderStyle

--- driver/js/examples/hippy-react-demo/src/modules/Clipboard/index.jsx
+++ driver/js/examples/hippy-react-demo/src/modules/Clipboard/index.jsx
@@ -13,6 +13,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     height: 40,
     borderWidth: 1,
+    borderStyle: 'solid',
     borderColor: '#e0e0e0',
     borderRadius: 2,
     backgroundColor: '#fafafa',
