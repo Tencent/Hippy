488503807de6fc6e4f1e25fda0b1abba0c866ab8
siguangli <siguangli@qq.com>
2022-07-20 21:52:50 +0800

    fix(android): unsupported horizontal dynamic setting

--- android/sdk/src/main/java/com/tencent/mtt/hippy/views/hippylist/HippyRecyclerViewController.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/views/hippylist/HippyRecyclerViewController.java
@@ -132,7 +132,7 @@ public class HippyRecyclerViewController<HRW extends HippyRecyclerViewWrapper> e
         return new ListViewRenderNode(id, props, className, hippyRootView, controllerManager, lazy);
     }
 
-    @HippyControllerProps(name = "horizontal", defaultType = HippyControllerProps.BOOLEAN)
+    @HippyControllerProps(name = "horizontal-unsupported", defaultType = HippyControllerProps.BOOLEAN)
     public void setHorizontalEnable(final HRW viewWrapper, boolean flag) {
         LayoutManager layoutManager = viewWrapper.getRecyclerView().getLayoutManager();
         if (!(layoutManager instanceof LinearLayoutManager)) {
