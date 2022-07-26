6efcc4cc051db44fd5f136c94a454e0a8d660fbb
ilikethese <chenli@chenlideMacBook-Pro.local>
2022-07-26 16:07:56 +0800

    feat(dom): implement operator<< function to DomNode

--- dom/src/dom/tools/tools.cc
+++ dom/src/dom/tools/tools.cc
@@ -9,12 +9,6 @@ namespace hippy {
 inline namespace dom {
 inline namespace tools {
 
-const std::map<int32_t, std::string> kRelativeTypeMap = {
-    {-1, "kFront"},
-    {0, "kDefault"},
-    {1, "kBack"},
-};
-
 Tools::Tools() : identifier_("") {}
 
 Tools::Tools(std::string identifier) : identifier_(identifier) {}
@@ -55,40 +49,7 @@ void Tools::Log(const DomInfo& node) {
 
 std::string Tools::Message(const DomInfo& node) {
   std::stringstream stream;
-  auto dom_node = node.dom_node;
-  auto ref_info = node.ref_info;
-  stream << "{";
-  if (ref_info != nullptr) {
-    stream << "\"ref info\": {";
-    stream << "\"ref_id\": " << ref_info->ref_id << ", ";
-    stream << "\"relative_to_ref\": \"" << kRelativeTypeMap.find(ref_info->relative_to_ref)->second << "\"";
-    stream << "}, ";
-  }
-  if (dom_node != nullptr) {
-    stream << "\"dom node\": {";
-    stream << "\"id\": " << dom_node->GetId() << ", ";
-    stream << "\"pid\": " << dom_node->GetPid() << ", ";
-    stream << "\"view name\": \"" << dom_node->GetViewName() << "\", ";
-    auto style = dom_node->GetStyleMap();
-    if (style != nullptr) {
-      stream << "\"style\": {";
-      for (const auto& s : *style) {
-        stream << "\"" << s.first << "\": " << *s.second << ", ";
-      }
-      stream << "}, ";
-    }
-    auto ext = dom_node->GetExtStyle();
-    if (ext != nullptr) {
-      stream << "\"ext style\": {";
-      for (const auto& e : *ext) {
-        stream << "\"" << e.first << "\": " << *e.second << ", ";
-      }
-      stream << "}, ";
-    }
-    stream << "}";
-  }
-  stream << "}";
-
+  stream << node;
   return stream.str();
 }
 
