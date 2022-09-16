c3202b1c0fb356504da7e89b243054ccf0be52b3
zoomchan-cxj <zoom_chan@163.com>
2022-09-16 18:02:57 +0800

    fix(core): revert hippy1.x compatible code

--- core/js/global/android/global.js
+++ core/js/global/android/global.js
@@ -24,3 +24,8 @@ __GLOBAL__.moduleCallList = {};
 __GLOBAL__.canRequestAnimationFrame = true;
 __GLOBAL__.requestAnimationFrameId = 0;
 __GLOBAL__.requestAnimationFrameQueue = {};
+// compatible for hippy1.x
+__GLOBAL__.nodeIdCache = {};
+__GLOBAL__.nodeTreeCache = {};
+__GLOBAL__.nodeParamCache = {}; // Not necessary for Android, but need for clean.
+__GLOBAL__.destroyInstanceList = {};
