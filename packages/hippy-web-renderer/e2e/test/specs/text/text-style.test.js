482207254018d0cbe693cf820782fa9185365282
pba-cra <syb233@icloud.com>
2022-11-02 15:13:07 +0800

    test(addcase): add test case and license

--- packages/hippy-web-renderer/e2e/test/specs/text/text-style.test.js
+++ packages/hippy-web-renderer/e2e/test/specs/text/text-style.test.js
@@ -1,8 +1,8 @@
 const { expect } = chai;
 
 describe('text', () => {
-  it('text font', (done)  => {
-    __e2e__.route.replace('/text-font');
+  it('text style', (done)  => {
+    __e2e__.route.replace('/text-style');
     snapshot(0.2).then((resolve) => {
       expect(resolve).to.equal(true);
       done();
--- packages/hippy-web-renderer/e2e/test/specs/text/text-weight.test.js
+++ packages/hippy-web-renderer/e2e/test/specs/text/text-weight.test.js
@@ -1,8 +1,8 @@
 const { expect } = chai;
 
 describe('text', () => {
-  it('text font', (done)  => {
-    __e2e__.route.replace('/text-font');
+  it('text weight', (done)  => {
+    __e2e__.route.replace('/text-weight');
     snapshot(0.2).then((resolve) => {
       expect(resolve).to.equal(true);
       done();
