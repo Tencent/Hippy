55780ef58676a1183b4063d9fe970be5c889ac8b
zoomchan-cxj <zoom_chan@163.com>
2022-11-15 17:42:14 +0800

    fix(react,vue): fix getBoundingClientRect errMsg syntax

--- packages/hippy-react/src/modules/ui-manager-module.ts
+++ packages/hippy-react/src/modules/ui-manager-module.ts
@@ -225,8 +225,8 @@ function getBoundingClientRect(ref: Fiber, options: { relToContainer?: boolean }
     }
     trace('UIManagerModule', { nodeId, funcName: 'getBoundingClientRect', params: options });
     return Bridge.callNative('UIManagerModule', 'getBoundingClientRect', nodeId, options, (res: HippyTypes.LayoutEvent) => {
-      if (!res || res.errorMsg) {
-        return reject(new Error((res?.errorMsg) || 'getBoundingClientRect error with no response'));
+      if (!res || res.errMsg) {
+        return reject(new Error((res?.errMsg) || 'getBoundingClientRect error with no response'));
       }
       const { x, y, width, height } = res;
       let bottom: undefined | number = undefined;
