b760ecb3af16e4f90f66d487b7daf00fb1264467
ivanfanwu <ivanfanwu@tencent.com>
2022-11-23 20:22:06 +0800

    feat(devtools): update ios project name

--- framework/examples/ios-demo/cmakebuild.sh
+++ framework/examples/ios-demo/cmakebuild.sh
@@ -5,6 +5,6 @@ then
 	echo 'cmake is not installed, install cmake first'
 	exit 1
 fi
-rm -rf devtools_backend
-cmake ../../../devtools/devtools-integration/ios/CMakeLists.txt -B ./devtools_backend -G Xcode -DMODULE_TOOLS=YES -DCMAKE_TOOLCHAIN_FILE=./ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=YES -DENABLE_ARC=YES -DDEPLOYMENT_TARGET=11.0 -DENABLE_INSPECTOR=YES
+rm -rf DevtoolsBackend
+cmake ../../../devtools/devtools-integration/ios/CMakeLists.txt -B ./DevtoolsBackend -G Xcode -DMODULE_TOOLS=YES -DCMAKE_TOOLCHAIN_FILE=./ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=YES -DENABLE_ARC=YES -DDEPLOYMENT_TARGET=11.0 -DENABLE_INSPECTOR=YES
 echo 'build ends'
