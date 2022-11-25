7af15ca6ae041a4cd1a9ba30930b0bfa4f99febe
chestershen <chestershen@tencent.com>
2022-11-25 15:23:34 +0800

    feat(vue): add beforeRenderToNative hook to support computed style

--- packages/hippy-vue-css-loader/src/__tests__/color-parser.test.js
+++ packages/hippy-vue-css-loader/src/__tests__/color-parser.test.js
@@ -1,3 +1,23 @@
+/*
+ * Tencent is pleased to support the open source community by making
+ * Hippy available.
+ *
+ * Copyright (C) 2017-2022 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import test from 'ava';
 import translateColor from '../color-parser';
 
@@ -19,6 +39,7 @@ test('Test color translation', (t) => {
   t.is(translateColor('transparent'), 0);
   t.is(translateColor('blueviolet'), 4287245282);
   t.is(translateColor(4287245282), 3808397867);
+  t.is(translateColor('var(-Bg)'), 'var(-Bg)');
 });
 
 test('Test color translation error handle NaN', (t) => {
