bebc95534164e697b4ded4dd5d8b2fc0175db7d9
DMX <34561818+crazyming9528@users.noreply.github.com>
2022-12-26 10:35:10 +0800

    chore(docs,demo): update WebRenderer docs & demo (#2566)
    * chore(hippy-react-demo): remove hippy:dev from web:dev
    
    * chore(hippy-react-demo): fix image path error in web-renderer dev mode
    
    * docs(web): Update integration.md and package.json
    
    Co-authored-by: questguo <questguo@tencent.com>
    Co-authored-by: Zoom Chan <zoom_chan@163.com>

--- examples/hippy-react-demo/scripts/hippy-webpack.web-renderer.dev.js
+++ examples/hippy-react-demo/scripts/hippy-webpack.web-renderer.dev.js
@@ -36,7 +36,11 @@ module.exports = {
       template: path.resolve('./public/index.html'),
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        HOST: JSON.stringify(process.env.DEV_HOST || '127.0.0.1'),
+        PORT: JSON.stringify(process.env.DEV_PORT || 3000),
+      },
       __PLATFORM__: JSON.stringify(platform),
     }),
     new CaseSensitivePathsPlugin(),
