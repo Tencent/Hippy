14e26fff8dd26fd18a983f0919d2fb0c413793fd
maxli <maxli@tencent.com>
2022-12-26 16:09:36 +0800

    fix(android): debug reload not destroy dom manager

--- framework/android/src/main/java/com/tencent/mtt/hippy/HippyEngineManagerImpl.java
+++ framework/android/src/main/java/com/tencent/mtt/hippy/HippyEngineManagerImpl.java
@@ -68,7 +68,6 @@ import com.openhippy.connector.JsDriver.V8InitParams;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
 
@@ -579,11 +578,17 @@ public abstract class HippyEngineManagerImpl extends HippyEngineManager implemen
         if (mCurrentState != EngineState.INITING) {
             mCurrentState = EngineState.ONRESTART;
         }
+        DomManager domManager = null;
         if (onReLoad && mEngineContext != null) {
-            mEngineContext.destroy(true);
+            if (mDebugMode) {
+                domManager = mEngineContext.getDomManager();
+                mEngineContext.destroy(true);
+            } else {
+                mEngineContext.destroy(false);
+            }
         }
         try {
-            mEngineContext = new HippyEngineContextImpl();
+            mEngineContext = new HippyEngineContextImpl(domManager);
         } catch (RuntimeException e) {
             LogUtils.e(TAG, "new HippyEngineContextImpl(): " + e.getMessage());
             notifyEngineInitialized(EngineInitStatus.STATUS_INIT_EXCEPTION, e);
@@ -713,9 +718,9 @@ public abstract class HippyEngineManagerImpl extends HippyEngineManager implemen
         @NonNull
         private final NativeRenderer mNativeRenderer;
         @NonNull
-        private final JsDriver mJsDriver;
-        @NonNull
         private final DomManager mDomManager;
+        @NonNull
+        private final JsDriver mJsDriver;
         @NonNull
         private final VfsManager mVfsManager;
         @Nullable
@@ -723,7 +728,7 @@ public abstract class HippyEngineManagerImpl extends HippyEngineManager implemen
         @Nullable
         volatile CopyOnWriteArrayList<HippyEngineLifecycleEventListener> mEngineLifecycleEventListeners;
 
-        public HippyEngineContextImpl() throws RuntimeException {
+        public HippyEngineContextImpl(@Nullable DomManager domManager) throws RuntimeException {
             mVfsManager = (mProcessors != null) ? new VfsManager(mProcessors) : new VfsManager();
             mVfsManager.setId(onCreateVfs(mVfsManager));
             DefaultProcessor processor = new DefaultProcessor(new HippyResourceLoader(this));
@@ -742,7 +747,7 @@ public abstract class HippyEngineManagerImpl extends HippyEngineManager implemen
             mBridgeManager = new HippyBridgeManagerImpl(this, mCoreBundleLoader,
                     getBridgeType(), enableV8Serialization, mDebugMode,
                     mServerHost, mGroupId, mThirdPartyAdapter, v8InitParams, mJsDriver);
-            mDomManager = new DomManager();
+            mDomManager = (domManager != null) ? domManager : new DomManager();
             mNativeRenderer = new NativeRenderer();
             mDomManager.attachToRenderer(mNativeRenderer);
             mNativeRenderer.attachToDom(mDomManager);
@@ -774,6 +779,12 @@ public abstract class HippyEngineManagerImpl extends HippyEngineManager implemen
             }
         }
 
+        @NonNull
+        DomManager getDomManager() {
+            return mDomManager;
+        }
+
+        @NonNull
         NativeRenderer getNativeRenderer() {
             return mNativeRenderer;
         }
@@ -960,7 +971,9 @@ public abstract class HippyEngineManagerImpl extends HippyEngineManager implemen
                 mDevtoolsManager.destroy(isReload);
             }
             mNativeRenderer.destroy();
-            mDomManager.destroy();
+            if (!isReload) {
+                mDomManager.destroy();
+            }
             mBridgeManager.destroy();
             mModuleManager.destroy();
             mVfsManager.destroy();
