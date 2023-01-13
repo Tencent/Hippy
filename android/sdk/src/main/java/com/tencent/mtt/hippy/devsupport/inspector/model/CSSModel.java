93ed91d53083a466777b708bff8c93f9da7a010d
ivanfanwu <ivanfanwu@tencent.com>
2023-01-13 17:44:45 +0800

    fix(devtools): remove regular expression injection

--- android/sdk/src/main/java/com/tencent/mtt/hippy/devsupport/inspector/model/CSSModel.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/devsupport/inspector/model/CSSModel.java
@@ -1,3 +1,18 @@
+/* Tencent is pleased to support the open source community by making Hippy available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tencent.mtt.hippy.devsupport.inspector.model;
 
 import com.tencent.mtt.hippy.HippyEngineContext;
@@ -415,8 +430,10 @@ public class CSSModel {
         result.append(split);
         continue;
       }
-      result
-        .append(split.replaceFirst(split.substring(0, 1), split.substring(0, 1).toUpperCase()));
+      result.append(split.substring(0, 1).toUpperCase());
+      if (split.length() > 1) {
+        result.append(split.substring(1));
+      }
     }
     return result.toString();
   }
@@ -434,8 +451,10 @@ public class CSSModel {
         result.append(split);
         continue;
       }
-      result.append(
-        split.replaceFirst(split.substring(0, 1), "-" + split.substring(0, 1).toLowerCase()));
+      result.append("-").append(split.substring(0, 1).toLowerCase());
+      if (split.length() > 1) {
+        result.append(split.substring(1));
+      }
     }
     return result.toString();
   }
