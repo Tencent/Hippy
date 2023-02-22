d7641a124ce579df450897fc610c5a85ed0392f4
iPel <pel20121221@gmail.com>
2023-02-22 12:40:40 +0800

    fix(android): textInput may throw IllegalStateException

--- renderer/native/android/src/main/java/com/tencent/mtt/hippy/views/hippylist/HippyRecyclerView.java
+++ renderer/native/android/src/main/java/com/tencent/mtt/hippy/views/hippylist/HippyRecyclerView.java
@@ -32,6 +32,7 @@ import com.tencent.mtt.hippy.utils.PixelUtil;
 import com.tencent.mtt.hippy.views.hippylist.recyclerview.helper.skikcy.IHeaderAttachListener;
 import com.tencent.mtt.hippy.views.hippylist.recyclerview.helper.skikcy.IHeaderHost;
 import com.tencent.mtt.hippy.views.hippylist.recyclerview.helper.skikcy.StickyHeaderHelper;
+import java.util.ArrayList;
 
 /**
  * Created  on 2020/12/22. Description
@@ -40,6 +41,7 @@ public class HippyRecyclerView<ADP extends HippyRecyclerListAdapter> extends Hip
         implements IHeaderAttachListener, HippyViewHolderAbandonListener {
 
     private static int DEFAULT_ITEM_VIEW_CACHE_SIZE = 4;
+    private static ArrayList<View> sTempViews;
     protected ADP listAdapter;
     protected boolean isEnableScroll = true;    //使能ListView的滚动功能
     protected StickyHeaderHelper stickyHeaderHelper;        //支持吸顶
@@ -447,6 +449,37 @@ public class HippyRecyclerView<ADP extends HippyRecyclerListAdapter> extends Hip
             stickyHeaderHelper.setStickViewListener(null);
         }
         viewStickEventHelper = null;
+    }
+
+    @Override
+    public View focusSearch(View focused, int direction) {
+        View result = super.focusSearch(focused, direction);
+        // {@link RecyclerView#focusSearch} may return not focusable view,
+        // cause IllegalStateException, so we verify again.
+        if (result == null || !verifyFocusable(result, direction)) {
+            return null;
+        }
+        return result;
+    }
+
+    private boolean verifyFocusable(@NonNull View view, int direction) {
+        boolean inTouchMode = isInTouchMode();
+        // first check whether self is able to take focus
+        if (inTouchMode ? view.isFocusableInTouchMode() : view.isFocusable()) {
+            return true;
+        }
+        // then check whether has focusable descendants
+        if (!(view instanceof ViewGroup)) {
+            return false;
+        }
+        if (sTempViews == null) {
+            sTempViews = new ArrayList<>();
+        }
+        view.addFocusables(sTempViews, direction, inTouchMode ? FOCUSABLES_TOUCH_MODE : FOCUSABLES_ALL);
+        boolean result = !sTempViews.isEmpty();
+        // clear up the static temp array
+        sTempViews.clear();
+        return result;
     }
 
     @Override
