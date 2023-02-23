56d1c34b6d67b23003fb77399c881386ea134473
iPel <pel20121221@gmail.com>
2023-02-23 15:22:04 +0800

    fix(android): fix unit test case (#2968)
    * fix(android): fix unit test case
    
    * fix(android): add license header

--- android/sdk/src/test/java/com/tencent/mtt/hippy/utils/UrlUtilsTest.java
+++ android/sdk/src/test/java/com/tencent/mtt/hippy/utils/UrlUtilsTest.java
@@ -1,3 +1,18 @@
+/* Tencent is pleased to support the open source community by making Hippy available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tencent.mtt.hippy.utils;
 
 import static org.junit.Assert.*;
@@ -20,8 +35,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
     "android.*",
 })
 public class UrlUtilsTest {
-  private final String URL_HTTPS = "https://user-images.githubusercontent.com/12878546/148736102-7cd9525b-aceb-41c6-a905-d3156219ef16.png";
-  private final String URL_HTTP = "https://user-images.githubusercontent.com/12878546/148736102-7cd9525b-aceb-41c6-a905-d3156219ef16.png";
+  private final String URL_HTTPS = "https://hippyjs.org/";
+  private final String URL_HTTP = "http://hippyjs.org/";
   private final String URL_FILE = "file://vendor.android.js";
 
   @Before
