9dc0f33e8e663163d64828ad4e93b7398454d55e
ilikethese <shenli0124@qq.com>
2023-02-24 12:05:20 +0800

    feat(layout): replace taitank layout and remove layout directory (#2850)
    * feat(layout): replace taitank layout and remove layout directory
    
    * feat(layout): remove code owners
    
    * feat(ios): replace taitank layout and remove layout directory
    
    * feat(ios): replace taitank layout and remove layout directory
    
    * feat(ios): replace taitank layout and remove layout directory
    
    * feat(ios): set taitank as default layout engine
    
    * feat(layout): update taitank layout version
    
    * feat(layout): remove unnecessary cmake content
    
    * fix(ios): remove unnessessary file
    
    * feat(dom): add GlobalPackagesModule for dom
    
    * feat(ios): make layout engines indenpendent subspec
    
    * feat(ios): make layout engines indenpendent subspec
    
    * feat(ios): make layout engines indenpendent subspec
    
    ---------
    
    Co-authored-by: ilikethese <chenli@chenlideMacBook-Pro.local>
    Co-authored-by: luomy <ozonelmy@163.com>

--- layout/engine/Flex.cpp
+++
@@ -1,5 +0,0 @@
-#include "Flex.h"
-
-const CSSDirection axisStart[4] = {CSSLeft, CSSRight, CSSTop, CSSBottom};
-const CSSDirection axisEnd[4] = {CSSRight, CSSLeft, CSSBottom, CSSTop};
-const Dimension axisDim[4] = {DimWidth, DimWidth, DimHeight, DimHeight};
