2224bd52209cb09c381d23cd817e67024b02d308
etkmao <etkmao@tecent.com>
2023-02-28 18:02:35 +0800

    fix(renderer): fix ImageViewNode param error crash

--- renderer/tdf/common/src/renderer/tdf/viewnode/image_view_node.cc
+++ renderer/tdf/common/src/renderer/tdf/viewnode/image_view_node.cc
@@ -87,7 +87,9 @@ void ImageViewNode::HandleStyleUpdate(const DomStyleMap &dom_style) {
     SetDefaultSrc(it->second->ToStringChecked());
   }
   if (auto it = dom_style.find(hippy::kImageSrc); it != dom_style.end()) {
-    SetSrc(it->second->ToStringChecked());
+    if (!it->second->IsUndefined()) {
+      SetSrc(it->second->ToStringChecked());
+    }
   }
 }
 
