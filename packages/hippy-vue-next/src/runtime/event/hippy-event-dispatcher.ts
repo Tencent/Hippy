d08affb8c3add1db81da3719a6859fbd604ae09b
zoomchan-cxj <zoom_chan@163.com>
2023-03-03 00:22:04 +0800

    feat(vue): add native event parameters

--- packages/hippy-vue-next/src/runtime/event/hippy-event-dispatcher.ts
+++ packages/hippy-vue-next/src/runtime/event/hippy-event-dispatcher.ts
@@ -116,16 +116,14 @@ const HippyEventDispatcher = {
     if (!nativeEvent) {
       return;
     }
-
-    const { id: targetNodeId, name: eventName } = nativeEvent;
+    const { id: targetNodeId, name: eventName, ...params } = nativeEvent;
     const targetNode = getNodeById(targetNodeId);
-
     if (!targetNode) {
       return;
     }
-
     const targetEventName = getVueEventName(eventName, targetNode);
     const targetEvent = new HippyEvent(targetEventName);
+    targetEvent.nativeParams = params;
     const { processEventData } = targetNode.component;
     if (processEventData) {
       processEventData(
@@ -148,23 +146,20 @@ const HippyEventDispatcher = {
     if (isInvalidNativeEvent(nativeEvent)) {
       return;
     }
-
     const [targetNodeId, eventName, params] = nativeEvent;
     if (typeof targetNodeId !== 'number' || typeof eventName !== 'string') {
       return;
     }
-
     const targetNode = getNodeById(targetNodeId);
     if (!targetNode) {
       return;
     }
-
     const targetEventName = getVueEventName(eventName, targetNode);
-
     // process layout event
     if (eventName === 'onLayout') {
       const { layout } = params;
       const targetLayoutEvent = new HippyLayoutEvent(targetEventName);
+      targetLayoutEvent.nativeParams = params ?? {};
       targetLayoutEvent.top = layout.y;
       targetLayoutEvent.left = layout.x;
       targetLayoutEvent.bottom = layout.y + layout.height;
@@ -175,7 +170,7 @@ const HippyEventDispatcher = {
       targetNode.dispatchEvent(targetLayoutEvent);
     } else {
       const targetEvent = new HippyEvent(targetEventName);
-
+      targetEvent.nativeParams = params ?? {};
       // other event processing, if the node itself has additional event processing logic, it also needs to be processed
       const { processEventData } = targetNode.component;
 
