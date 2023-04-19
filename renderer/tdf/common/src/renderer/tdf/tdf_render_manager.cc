5663e5d0c1b4b2a84a195d117a631da527249e72
etkmao <etkmao@tecent.com>
2023-04-19 18:19:49 +0800

    fix(renderer): adaption for hippy3.0

--- renderer/tdf/common/src/renderer/tdf/tdf_render_manager.cc
+++ renderer/tdf/common/src/renderer/tdf/tdf_render_manager.cc
@@ -270,7 +270,7 @@ void TDFRenderManager::UpdateLayout(std::weak_ptr<RootNode> root_node,
 }
 
 void TDFRenderManager::MoveRenderNode(std::weak_ptr<RootNode> root_node, std::vector<int32_t>&& moved_ids,
-                                      int32_t from_pid, int32_t to_pid) {
+                                      int32_t from_pid, int32_t to_pid, int32_t index) {
   FOOTSTONE_DCHECK(false);
 }
 
