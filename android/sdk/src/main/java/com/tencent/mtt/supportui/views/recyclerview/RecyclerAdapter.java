15a5e6a4c25715241538c91fdc8b48163c5117b7
iPel <pel20121221@gmail.com>
2023-05-25 17:01:46 +0800

    fix(android): waterfall scrolling error with small content height

--- android/sdk/src/main/java/com/tencent/mtt/supportui/views/recyclerview/RecyclerAdapter.java
+++ android/sdk/src/main/java/com/tencent/mtt/supportui/views/recyclerview/RecyclerAdapter.java
@@ -1,3 +1,18 @@
+/* Tencent is pleased to support the open source community by making Hippy available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tencent.mtt.supportui.views.recyclerview;
 
 import static com.tencent.mtt.supportui.views.recyclerview.RecyclerViewItem.ITEM_VIEW_DEFAULT_HEIGHT;
@@ -1044,10 +1059,10 @@ public abstract class RecyclerAdapter extends RecyclerViewBase.Adapter<RecyclerV
 		}
 	}
 
-	public int getDefaultFooterHeight()
-	{
-		return 108;
-	}
+	// public int getDefaultFooterHeight()
+	// {
+	// 	return 108;
+	// }
 
 	// 用户上拉触发了加载，footer开始转圈，业务开始拉去数据
 	@Override
