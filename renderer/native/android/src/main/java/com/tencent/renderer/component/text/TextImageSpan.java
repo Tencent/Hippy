4537608d2f6b265306f401762fbe000c738b83db
iPel <pel20121221@gmail.com>
2023-05-26 21:35:23 +0800

    feat(android): revert legacy image verticalAlignment

--- renderer/native/android/src/main/java/com/tencent/renderer/component/text/TextImageSpan.java
+++ renderer/native/android/src/main/java/com/tencent/renderer/component/text/TextImageSpan.java
@@ -19,6 +19,7 @@ package com.tencent.renderer.component.text;
 import static com.tencent.renderer.utils.EventUtils.EVENT_IMAGE_LOAD_ERROR;
 import static com.tencent.renderer.utils.EventUtils.EVENT_IMAGE_ON_LOAD;
 
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -34,6 +35,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.SystemClock;
 import android.text.TextUtils;
+import android.text.style.DynamicDrawableSpan;
 import android.text.style.ImageSpan;
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
@@ -49,18 +51,25 @@ import com.tencent.renderer.node.ImageVirtualNode;
 import com.tencent.renderer.node.TextVirtualNode;
 import com.tencent.renderer.utils.EventUtils.EventType;
 import com.tencent.vfs.UrlUtils;
+import java.lang.reflect.Field;
 
 public class TextImageSpan extends ImageSpan {
 
     private static final int STATE_UNLOAD = 0;
     private static final int STATE_LOADING = 1;
     private static final int STATE_LOADED = 2;
+    private final boolean mUseLegacy;
+    @Nullable
     private final String mVerticalAlign;
     private final int mRootId;
     private final int mId;
     private final int mAncestorId;
     private final int mWidth;
     private final int mHeight;
+    @Deprecated
+    private final int mLeft;
+    @Deprecated
+    private final int mTop;
     private final int mMarginLeft;
     private final int mMarginTop;
     private final int mMarginRight;
@@ -76,6 +85,9 @@ public class TextImageSpan extends ImageSpan {
     private Drawable mSrcDrawable;
     @Nullable
     private Movie mGifMovie;
+    @Deprecated
+    @Nullable
+    private LegacyIAlignConfig mAlignConfig;
     @Nullable
     private Paint mGifPaint;
     private float mHeightRate = 0;
@@ -85,18 +97,22 @@ public class TextImageSpan extends ImageSpan {
 
     public TextImageSpan(Drawable drawable, String source, @NonNull ImageVirtualNode node,
             @NonNull NativeRender nativeRenderer) {
-        super(drawable, source);
+        super(drawable, source, node.getVerticalAlignment());
         mNativeRenderer = nativeRenderer;
         mRootId = node.getRootId();
         mId = node.getId();
         mAncestorId = node.getAncestorId();
         mWidth = node.getWidth();
         mHeight = node.getHeight();
+        mLeft = node.getLeft();
+        mTop = node.getTop();
         mMarginLeft = node.getMarginLeft();
         mMarginRight = node.getMarginRight();
         mMarginTop = node.getMarginTop();
         mMarginBottom = node.getMarginBottom();
         mVerticalAlign = node.getVerticalAlign();
+        mUseLegacy = mVerticalAlign == null;
+        mAlignConfig = mUseLegacy ? LegacyIAlignConfig.fromVerticalAlignment(node.getVerticalAlignment()) : null;
         mTintColor = node.getTintColor();
         int backgroundColor = node.getBackgroundColor();
         if (backgroundColor != Color.TRANSPARENT) {
@@ -115,6 +131,9 @@ public class TextImageSpan extends ImageSpan {
     @Override
     public int getSize(@NonNull Paint paint, CharSequence text, int start, int end,
             @Nullable FontMetricsInt fm) {
+        if (mUseLegacy) {
+            return legacyGetSize(paint, text, start, end, fm);
+        }
         if (mHeightRate > 0) {
             if (mHeight == 0) {
                 mMeasuredWidth = mMeasuredHeight = 0;
@@ -135,13 +154,27 @@ public class TextImageSpan extends ImageSpan {
         return mMeasuredWidth + mMarginLeft + mMarginRight;
     }
 
+    private int legacyGetSize(@NonNull Paint paint, CharSequence text, int start, int end,
+            @Nullable FontMetricsInt fm) {
+        if (mGifMovie != null) {
+            return super.getSize(paint, text, start, end, fm);
+        }
+        Drawable drawable = getDrawable();
+        return mAlignConfig.getSize(paint, text, start, end, fm, drawable);
+    }
+
     public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y,
             int bottom, Paint paint) {
+        if (mUseLegacy) {
+            legacyDraw(canvas, text, start, end, x, top, y, bottom, paint);
+            return;
+        }
         if (mMeasuredWidth == 0 || mMeasuredHeight == 0) {
             return;
         }
         canvas.save();
         int transY;
+        assert mVerticalAlign != null;
         switch (mVerticalAlign) {
             case TextVirtualNode.V_ALIGN_TOP:
                 transY = top + mMarginTop;
@@ -180,9 +213,43 @@ public class TextImageSpan extends ImageSpan {
         canvas.restore();
     }
 
+    private void legacyDraw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y,
+            int bottom, Paint paint) {
+        int transY;
+        Paint.FontMetricsInt fm = paint.getFontMetricsInt();
+        if (mGifMovie != null) {
+            int width = (mWidth == 0) ? mGifMovie.width() : mWidth;
+            int height = (mHeight == 0) ? mGifMovie.height() : mHeight;
+            transY = (y + fm.descent + y + fm.ascent) / 2 - height / 2;
+            legacyDrawGIF(canvas, x + mLeft, transY + mTop, width, height);
+        } else {
+            Drawable drawable = getDrawable();
+            mAlignConfig.draw(canvas, text, start, end, x, top, y, bottom, paint, drawable, mBackgroundPaint);
+        }
+    }
+
+    @Deprecated
+    @SuppressWarnings("unused")
+    public void setDesiredSize(int width, int height) {
+        if (mUseLegacy) {
+            mAlignConfig.setDesiredSize(width, height);
+        }
+    }
+
     @SuppressWarnings("unused")
     public void setActiveSizeWithRate(float heightRate) {
         mHeightRate = heightRate;
+        if (mUseLegacy) {
+            mAlignConfig.setActiveSizeWithRate(heightRate);
+        }
+    }
+
+    @Deprecated
+    @SuppressWarnings("unused")
+    public void setMargin(int marginLeft, int marginRight) {
+        if (mUseLegacy) {
+            mAlignConfig.setMargin(marginLeft, marginRight);
+        }
     }
 
     protected boolean shouldUseFetchImageMode(String url) {
@@ -240,12 +307,37 @@ public class TextImageSpan extends ImageSpan {
         mGifMovie.setTime(progress);
     }
 
+    private void legacyDrawGIF(Canvas canvas, float left, float top, int width, int height) {
+        if (mGifMovie == null) {
+            return;
+        }
+        updateGifTime();
+        if (mBackgroundPaint != null) {
+            canvas.drawRect(left, top, left + width, top + height, mBackgroundPaint);
+        }
+        final float mGifScaleX = width / (float) mGifMovie.width();
+        final float mGifScaleY = height / (float) mGifMovie.height();
+        final float x = (mGifScaleX != 0) ? left / mGifScaleX : left;
+        final float y = (mGifScaleY != 0) ? top / mGifScaleY : top;
+        int progress = mGifProgress > Integer.MAX_VALUE ? 0 : (int) mGifProgress;
+        mGifMovie.setTime(progress);
+        canvas.save();
+        canvas.scale(mGifScaleX, mGifScaleY);
+        mGifMovie.draw(canvas, x, y, mGifPaint);
+        canvas.restore();
+        postInvalidateDelayed(40);
+    }
+
     @MainThread
     private void postInvalidateDelayed(long delayMilliseconds) {
         mNativeRenderer.postInvalidateDelayed(mRootId, mAncestorId, delayMilliseconds);
     }
 
     private void shouldReplaceDrawable(@NonNull ImageDataHolder imageHolder) {
+        if (mUseLegacy) {
+            legacyShouldReplaceDrawable(imageHolder);
+            return;
+        }
         mSrcDrawable = null;
         mGifMovie = null;
         mGifPaint = null;
@@ -260,6 +352,44 @@ public class TextImageSpan extends ImageSpan {
             }
             drawable.setBounds(0, 0, mWidth, mHeight);
             mSrcDrawable = drawable;
+        } else if (imageHolder.isAnimated()) {
+            mGifMovie = imageHolder.getGifMovie();
+            if (mTintColor != Color.TRANSPARENT) {
+                mGifPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                mGifPaint.setColorFilter(new PorterDuffColorFilter(mTintColor, PorterDuff.Mode.SRC_ATOP));
+            }
+        }
+        imageHolder.attached();
+        postInvalidateDelayed(0);
+    }
+
+    @SuppressLint("DiscouragedPrivateApi")
+    private void legacyShouldReplaceDrawable(@NonNull ImageDataHolder imageHolder) {
+        Drawable imageDrawable = imageHolder.getDrawable();
+        Bitmap bitmap = imageHolder.getBitmap();
+        if (imageDrawable != null || bitmap != null) {
+            Resources resources = ContextHolder.getAppContext().getResources();
+            Drawable drawable = imageDrawable != null ? imageDrawable :
+                    new BitmapDrawable(resources, bitmap);
+            if (mTintColor != Color.TRANSPARENT) {
+                drawable.setColorFilter(new PorterDuffColorFilter(mTintColor, PorterDuff.Mode.SRC_ATOP));
+            }
+            int w = (mWidth == 0) ? drawable.getIntrinsicWidth() : mWidth;
+            int h = (mHeight == 0) ? drawable.getIntrinsicHeight() : mHeight;
+            drawable.setBounds(0, 0, w, h);
+            try {
+                //noinspection JavaReflectionMemberAccess
+                final Field drawableField = ImageSpan.class.getDeclaredField("mDrawable");
+                drawableField.setAccessible(true);
+                drawableField.set(TextImageSpan.this, drawable);
+                //noinspection JavaReflectionMemberAccess
+                final Field drawableRefField = DynamicDrawableSpan.class
+                        .getDeclaredField("mDrawableRef");
+                drawableRefField.setAccessible(true);
+                drawableRefField.set(TextImageSpan.this, null);
+            } catch (IllegalAccessException | NoSuchFieldException ignored) {
+                // Reflective access likely to remove in future Android releases
+            }
         } else if (imageHolder.isAnimated()) {
             mGifMovie = imageHolder.getGifMovie();
             if (mTintColor != Color.TRANSPARENT) {
