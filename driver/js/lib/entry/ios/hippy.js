1b8013ab9def0990c0c9e9530b72d0e89cc80f53
etkmao <etkmao@tecent.com>
2023-08-07 16:03:02 +0800

    fix(perf): support fps calculation for js

--- driver/js/lib/entry/ios/hippy.js
+++ driver/js/lib/entry/ios/hippy.js
@@ -34,10 +34,9 @@ require('../../global/Storage.js'); // localStorageAsync
 require('../../global/Dimensions.js'); // Hippy.device.window Hippy.device.screen Hippy.device.pixelRatio
 require('../../global/UtilsModule.js'); // Hippy.device.vibrate Hippy.device.cancelVibrate
 require('../../global/ios/global.js'); // set __GLOBAL__ object
-require('../../modules/ios/jsTimersExecution.js'); // ios module for .h build
 require('../../bridge/ios/native2js.js');
 require('../../global/Event.js'); // register global events callback
-require('../../global/ios/requestAnimationFrame.js'); // requestAnimationFrame cancelAnimationFrame
+require('../../global/AnimationFrameModule.js'); // requestAnimationFrame cancelAnimationFrame
 require('../../global/ios/Turbo.js'); // turbo
 // alias
 global.localStorage = Hippy.asyncStorage;
