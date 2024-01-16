3a76e4faf505514415335a3ff0f3885dae85a093
luciolong <luciolong@tencent.com>
2024-01-16 11:50:10 +0800

    fix(voltron): resolve remoteServerUrl parse version id failed

--- framework/voltron/test/flutter_render_test.dart
+++ framework/voltron/test/flutter_render_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:voltron/inspector/dev_remote_server_data.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -14,5 +15,12 @@ void main() {
   test('dynamicFuncCall', () {
     Function function = addNum;
     expect(Function.apply(function, [1, 2]), 3);
+  });
+
+  test('dev_server_data', () {
+    final data = DevRemoteServerData('http://1.1.1.1:8080/abcdefg/index.bundle?debugUrl=ws%3A%2F%2F0.0.0.0%3A8080%2Fdebugger-proxy');
+    expect(data.isValid(), true);
+    expect(data.getVersionId(), 'abcdefg');
+    expect(data.getHost(), '1.1.1.1:8080');
   });
 }
