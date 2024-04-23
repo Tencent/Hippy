e4d2df827d074fe0d1d328fb24ba3b370da56480
wwwcg <isarraypro@gmail.com>
2024-04-23 15:56:52 +0800

    chore(ios): add SecondaryBundleDidLoadNotification for compatibility

--- renderer/native/ios/renderer/HippyRootView.h
+++ renderer/native/ios/renderer/HippyRootView.h
@@ -47,6 +47,10 @@ typedef NS_ENUM(NSInteger, HippyRootViewSizeFlexibility) {
 /// is a good indicator that the application is ready to use.
 extern NSString *const HippyContentDidAppearNotification;
 
+/// Business bundle loading completion notification
+/// This notification is for compatibility with hippy2 and is not recommended for further use
+extern NSString *const HippySecondaryBundleDidLoadNotification DEPRECATED_MSG_ATTRIBUTE("use HippyJavaScriptDidLoadNotification");
+
 
 /// Native view used to host Hippy-managed views within the app.
 /// Can be used just like any ordinary UIView.
