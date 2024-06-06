67fadebf6471eb51102e245e37478370807e8384
wwwcg <isarraypro@gmail.com>
2024-06-06 17:18:09 +0800

    fix(ios): resolve exception where touchView may be nil

--- ios/sdk/base/HippyTouchHandler.m
+++ ios/sdk/base/HippyTouchHandler.m
@@ -117,7 +117,9 @@ typedef void (^ViewBlock)(UIView *view, BOOL *stop);
         UIView *touchView = [touch view];
         CGPoint locationPoint = [touch locationInView:touchView];
         touchView = touchView?:[self.view.window hitTest:locationPoint withEvent:event];
-        [_touchBeganViews addObject:touchView];
+        if (touchView) {
+            [_touchBeganViews addObject:touchView];
+        }
         NSDictionary *result = [self responseViewForAction:@[@"onPressIn", @"onTouchDown", @"onClick", @"onLongClick"] inView:touchView
                                                    atPoint:locationPoint];
 
