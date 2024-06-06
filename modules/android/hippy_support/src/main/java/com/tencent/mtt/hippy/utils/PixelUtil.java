4cec8d47d82baa0c6a4514a0a3c84c0cbb6a25d6
siguangli <maxli@tencent.com>
2024-06-06 17:53:01 +0800

    fix(android): use screen density for dp to px

--- modules/android/hippy_support/src/main/java/com/tencent/mtt/hippy/utils/PixelUtil.java
+++ modules/android/hippy_support/src/main/java/com/tencent/mtt/hippy/utils/PixelUtil.java
@@ -16,8 +16,11 @@
 
 package com.tencent.mtt.hippy.utils;
 
+import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class PixelUtil {
 
@@ -25,7 +28,11 @@ public class PixelUtil {
 
     public static DisplayMetrics getMetrics() {
         if (sDisplayMetrics == null) {
-            sDisplayMetrics = ContextHolder.getAppContext().getResources().getDisplayMetrics();
+            sDisplayMetrics = new DisplayMetrics();
+            WindowManager windowManager = (WindowManager) ContextHolder.getAppContext()
+                    .getSystemService(Context.WINDOW_SERVICE);
+            Display defaultDisplay = windowManager.getDefaultDisplay();
+            defaultDisplay.getRealMetrics(sDisplayMetrics);
         }
         return sDisplayMetrics;
     }
