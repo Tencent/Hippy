a1c2eac58390311ef55473acdfa350865dfa4ad3
zealotchen <zealotchen@tencent.com>
2024-06-12 16:30:50 +0800

    chore(release): released 2.17.4

--- ios/sdk/base/HippyBridge.mm
+++ ios/sdk/base/HippyBridge.mm
@@ -41,7 +41,7 @@ NSString *const HippyJavaScriptDidLoadNotification = @"HippyJavaScriptDidLoadNot
 NSString *const HippyJavaScriptDidFailToLoadNotification = @"HippyJavaScriptDidFailToLoadNotification";
 NSString *const HippyDidInitializeModuleNotification = @"HippyDidInitializeModuleNotification";
 NSString *const HippyBusinessDidLoadNotification = @"HippyBusinessDidLoadNotification";
-NSString *const _HippySDKVersion = @"2.17.3";
+NSString *const _HippySDKVersion = @"2.17.4";
 
 static NSMutableArray<Class> *HippyModuleClasses;
 NSArray<Class> *HippyGetModuleClasses(void) {
