73e814eff1e8ccb00b6f949e8901e2b4749b183e
wwwcg <isarraypro@gmail.com>
2024-08-06 18:29:27 +0800

    refactor(ios): remove unused data structure in waterfall item

--- renderer/native/ios/renderer/component/waterfalllist/HippyShadowListView.h
+++ renderer/native/ios/renderer/component/waterfalllist/HippyShadowListView.h
@@ -25,30 +25,12 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WaterfallItemChangeContext : NSObject<NSCopying>
-
-- (NSHashTable<__kindof HippyShadowView *> *)addedItems;
-- (NSHashTable<__kindof HippyShadowView *> *)frameChangedItems;
-- (NSSet<__kindof HippyShadowView *> *)deletedItems;
-- (NSHashTable<__kindof HippyShadowView *> *)movedItems;
-
-/// Clear all items recorded.
-- (void)clear;
-
-/// Whether has changed item.
-- (BOOL)hasChanges;
-
-/// Get all chaned items.
-- (NSSet<HippyShadowView *> *)allChangedItems;
-
-@end
-
+/// ListView's shadowView
 @interface HippyShadowListView : HippyShadowView <HippyShadowWaterfallItemFrameChangedProtocol>
 
-///// Whether current ShadowList is dirty.
-//@property (nonatomic, assign) BOOL isDirty;
-
-@property(nonatomic, readonly, strong)WaterfallItemChangeContext *itemChangeContext;
+/// Whether current ShadowList is dirty.
+/// Used to determine whether the list needs reload.
+@property (nonatomic, assign) BOOL isDirty;
 
 @end
 
