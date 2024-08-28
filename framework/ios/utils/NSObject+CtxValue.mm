42f9de055bb1e1a45d086099e3e0b6a73f3365c7
wwwcg <isarraypro@gmail.com>
2024-08-28 15:02:04 +0800

    fix(ios): resolve conversion error between NSString and CtxValue
    add unit test for data conversion

--- framework/ios/utils/NSObject+CtxValue.mm
+++ framework/ios/utils/NSObject+CtxValue.mm
@@ -152,20 +152,17 @@ id ObjectFromCtxValue(CtxPtr context, CtxValuePtr value) {
         if (context->IsString(value)) {
             footstone::string_view view;
             if (context->GetValueString(value, &view)) {
-                view = footstone::StringViewUtils::ConvertEncoding(view, footstone::string_view::Encoding::Utf16);
+                view = footstone::StringViewUtils::CovertToUtf16(view, view.encoding());
                 footstone::string_view::u16string &u16String = view.utf16_value();
-                NSString *string =
-                    [NSString stringWithCharacters:(const unichar *)u16String.c_str() length:u16String.length()];
+                NSString *string = [NSString stringWithCharacters:(const unichar *)u16String.c_str() length:u16String.length()];
                 return string;
             }
-        }
-        else if (context->IsNumber(value)) {
+        } else if (context->IsNumber(value)) {
             double number = 0;
             if (context->GetValueNumber(value, &number)) {
                 return @(number);
             }
-        }
-        else if (context->IsArray(value)) {
+        } else if (context->IsArray(value)) {
             uint32_t length = context->GetArrayLength(value);
             NSMutableArray *array = [NSMutableArray arrayWithCapacity:length];
             for (uint32_t index = 0; index < length; index++) {
@@ -183,8 +180,7 @@ id ObjectFromCtxValue(CtxPtr context, CtxValuePtr value) {
             if (context->GetByteBuffer(value, &bytes, length, type)) {
                 return [NSData dataWithBytes:bytes length:length];
             }
-        }
-        else if (context->IsObject(value)) {
+        } else if (context->IsObject(value)) {
             std::unordered_map<CtxValuePtr, CtxValuePtr> map;
             if (context->GetEntriesFromObject(value, map)) {
                 NSMutableDictionary *dictionary = [NSMutableDictionary dictionaryWithCapacity:map.size()];
@@ -194,17 +190,19 @@ id ObjectFromCtxValue(CtxPtr context, CtxValuePtr value) {
                     if (!flag) {
                         continue;
                     }
-                    const auto &u16String = footstone::StringViewUtils::CovertToUtf16(string_view, string_view.encoding()).utf16_value();
-                    NSString *string = [NSString stringWithCharacters:(const unichar *)u16String.c_str() length:u16String.length()];
+                    // Note that reference value (const auto &) cannot be used directly here,
+                    // since the temporary string_view object will destruct the uft16 value.
+                    // a wrong example is:
+                    // const auto &u16Str = footstone::StringViewUtils::CovertToUtf16(string_view, string_view.encoding()).utf16_value();
+                    string_view = footstone::StringViewUtils::CovertToUtf16(string_view, string_view.encoding());
+                    footstone::string_view::u16string &u16Str = string_view.utf16_value();
+                    NSString *string = [NSString stringWithCharacters:(const unichar *)u16Str.c_str() length:u16Str.length()];
                     auto &value = it.second;
                     id obj = ObjectFromCtxValue(context, value);
                     [dictionary setObject:obj forKey:string];
                 }
                 return [dictionary copy];
             }
-        }
-        else {
-
         }
         return [NSNull null];
     }
