02c82db565d219c083e13c71409db3fb748aeab1
wwwcg <isarraypro@gmail.com>
2024-10-14 20:25:18 +0800

    chore(demo): update list demo in both react and vue

--- driver/js/examples/hippy-vue-next-demo/src/components/demo/demo-list.vue
+++ driver/js/examples/hippy-vue-next-demo/src/components/demo/demo-list.vue
@@ -78,7 +78,6 @@
       </li>
     </ul>
     <div
-      v-if="Platform === 'android'"
       :style="{
         position: 'absolute',
         right: 20,
@@ -114,7 +113,7 @@
 </template>
 
 <script lang="ts">
-import { type ListViewEvent, Native } from '@hippy/vue-next';
+import { type ListViewEvent } from '@hippy/vue-next';
 import { defineComponent, ref, onMounted, type Ref } from '@vue/runtime-core';
 
 const STYLE_LOADING = 100;
@@ -269,7 +268,6 @@ export default defineComponent({
       list,
       STYLE_LOADING,
       horizontal,
-      Platform: Native.Platform,
       onAppear,
       onDelete,
       onDisappear,
