a3353172cfb624fed1cbee3663aee4de8e446b5b
zealotchen <zealotchen@tencent.com>
2024-11-06 21:37:33 +0800

    fix(js): add NamedModulesPlugin

--- driver/js/examples/hippy-react-demo/scripts/hippy-webpack.ios-vendor.js
+++ driver/js/examples/hippy-react-demo/scripts/hippy-webpack.ios-vendor.js
@@ -18,6 +18,7 @@ module.exports = {
     library: 'hippyReactBase',
   },
   plugins: [
+    new webpack.NamedModulesPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       __PLATFORM__: JSON.stringify(platform),
--- driver/js/examples/hippy-vue-next-demo/scripts/hippy-webpack.android.js
+++ driver/js/examples/hippy-vue-next-demo/scripts/hippy-webpack.android.js
@@ -31,6 +31,7 @@ module.exports = {
     // publicPath: 'https://xxx/hippy/hippyVueNextDemo/',
   },
   plugins: [
+    new webpack.NamedModulesPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       __PLATFORM__: JSON.stringify(platform),
