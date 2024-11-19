7763e560baac4ebeb8e1867f3615be392f35732c
wwwcg <isarraypro@gmail.com>
2024-11-19 16:12:22 +0800

    refactor(ios): inefficient code cleanup

--- modules/ios/domutils/HippyDomUtils.mm
+++ modules/ios/domutils/HippyDomUtils.mm
@@ -20,50 +20,27 @@
  * limitations under the License.
  */
 
-#import <Foundation/Foundation.h>
-
 #import "HippyDomUtils.h"
 #import "HippyFootstoneUtils.h"
 
-#include "dom/dom_listener.h"
-#include "dom/dom_node.h"
-#include "footstone/hippy_value.h"
-
-CGRect CGRectMakeFromLayoutResult(hippy::LayoutResult result) {
-    return CGRectMake(result.left, result.top, result.width, result.height);
-}
-
-UIEdgeInsets UIEdgeInsetsFromLayoutResult(hippy::LayoutResult result) {
-    return UIEdgeInsetsMake(result.paddingTop, result.paddingLeft, result.paddingBottom, result.paddingRight);
-}
-
-CGSize CGSizeMakeFromLayoutResult(hippy::LayoutResult result) {
-    return CGSizeMake(result.width, result.height);
-}
-
-CGRect CGRectMakeFromDomNode(const std::shared_ptr<hippy::DomNode> &domNode) {
-    return CGRectMakeFromLayoutResult(domNode->GetLayoutResult());
-}
-
-NSDictionary *StylesFromDomNode(const std::shared_ptr<hippy::DomNode> &domNode) {
+NSDictionary *HippyStylesFromDomNode(const std::shared_ptr<hippy::DomNode> &domNode) {
     auto &styles = domNode->GetStyleMap();
     auto &extStyles = domNode->GetExtStyle();
     auto capacity = 0;
-
     if (styles) {
-      capacity += styles->size();
+        capacity += styles->size();
     }
     if (extStyles) {
-      capacity += extStyles->size();
+        capacity += extStyles->size();
     }
     NSMutableDictionary *allStyles = [NSMutableDictionary dictionaryWithCapacity:capacity];
     if (styles) {
-      NSDictionary *dicStyles  = UnorderedMapDomValueToDictionary(styles);
-      [allStyles addEntriesFromDictionary:dicStyles];
+        NSDictionary *dicStyles = UnorderedMapDomValueToDictionary(styles);
+        [allStyles addEntriesFromDictionary:dicStyles];
     }
     if (extStyles) {
-      NSDictionary *dicExtStyles = UnorderedMapDomValueToDictionary(extStyles);
-      [allStyles addEntriesFromDictionary:dicExtStyles];
+        NSDictionary *dicExtStyles = UnorderedMapDomValueToDictionary(extStyles);
+        [allStyles addEntriesFromDictionary:dicExtStyles];
     }
-    return [allStyles copy];
+    return allStyles;
 }
