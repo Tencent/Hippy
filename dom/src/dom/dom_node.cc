1b6179441963a48ccd7043a08ef60602f15c8a01
etkmao <etkmao@tencent.com>
2025-03-10 15:31:14 +0800

    fix(core): fix DomNode cb_map erase bug

--- dom/src/dom/dom_node.cc
+++ dom/src/dom/dom_node.cc
@@ -223,7 +223,7 @@ std::tuple<float, float> DomNode::GetLayoutSize() {
 void DomNode::SetLayoutSize(float width, float height) {
   is_layout_width_nan_ = std::isnan(width) ? true : false;
   is_layout_height_nan_ = std::isnan(height) ? true : false;
-  
+
   layout_node_->SetWidth(width);
   layout_node_->SetHeight(height);
 }
@@ -435,7 +435,7 @@ CallFunctionCallback DomNode::GetCallback(const std::string& name, uint32_t id) 
   }
   auto it = func_cb_map_->find(name);
   if (it != func_cb_map_->end()) {
-    auto cb_map = it->second;
+    auto& cb_map = it->second;
     auto cb_it = cb_map.find(id);
     if (cb_it != cb_map.end()) {
       auto ret = std::move(cb_it->second);
