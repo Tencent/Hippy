b99d6b29aaf2d61f5728ef5bfc5faae7a47c388a
etkmao <etkmao@tencent.com>
2025-11-26 16:52:05 +0800

    feat(ohos): support logAdapter in EngineInitParams

--- framework/ohos/src/main/cpp/impl/entry.cc
+++ framework/ohos/src/main/cpp/impl/entry.cc
@@ -20,5 +20,45 @@
  *
  */
 
-#include <napi/native_api.h>
-#include <stdio.h>
+#include <js_native_api.h>
+#include <js_native_api_types.h>
+#include "oh_napi/oh_napi_object.h"
+#include "oh_napi/oh_napi_register.h"
+#include "oh_napi/oh_napi_task_runner.h"
+#include "oh_napi/ark_ts.h"
+#include "footstone/check.h"
+
+using OhNapiTaskRunner = hippy::OhNapiTaskRunner;
+
+static bool s_is_initialized_ = false;
+
+static napi_value SetNativeLogHandler(napi_env env, napi_callback_info info) {
+  ArkTS arkTs(env);
+  auto args = arkTs.GetCallbackArgs(info, 1);
+  auto ts_log_adapter = args[0];
+  auto ts_log_adapter_ref = arkTs.CreateReference(ts_log_adapter);
+
+  if (!s_is_initialized_) {
+    footstone::log::LogMessage::InitializeDelegate([env, ts_log_adapter_ref](
+        const std::ostringstream& stream,
+        footstone::log::LogSeverity severity) {
+      auto msg = stream.str();
+      OhNapiTaskRunner *taskRunner = OhNapiTaskRunner::Instance(env);
+      taskRunner->RunAsyncTask([env, ts_log_adapter_ref, severity, msg]() {
+        ArkTS arkTs(env);
+        std::vector<napi_value> args = {
+          arkTs.CreateInt(severity), // level
+          arkTs.CreateString("HippyLog"), // tag
+          arkTs.CreateString(msg), // msg
+        };
+        auto delegateObject = arkTs.GetObject(ts_log_adapter_ref);
+        delegateObject.Call("onReceiveLogMessage", args);
+      });
+    });
+    s_is_initialized_ = true;
+  }
+
+  return arkTs.GetUndefined();
+}
+
+REGISTER_OH_NAPI("Entry", "Entry_SetNativeLogHandler", SetNativeLogHandler)
